5fd824c8a40a73abe8cdae1fde7e0c12b8a4871c
Usha Devi <usha@mindssoft.com>
2015-11-14 19:19:17 +0530

    Api handler added for Country.

--- Design/Src-server/aparajitha/server/knowledge/handler.py
+++ Design/Src-server/aparajitha/server/knowledge/handler.py
@@ -97,12 +97,105 @@ class ChangeDomainStatusHandler(tornado.web.RequestHandler) :
             self.write(json.dumps(str(response)))
             self.finish()
 
+class GetCountryHandler(tornado.web.RequestHandler) :
+    def initialize(self, url, handler) :
+        self.url = url
+        self.handler = handler
+
+    @tornado.web.asynchronous
+    def post(self) :
+        try:
+            data = json.loads(self.request.body)
+            sessionToken = JSONHelper.getString(data, "session_token")
+            request = JSONHelper.getList(data, "request")
+            response = CountryList(sessionToken, request)
+        except Exception, e:
+            print e
+            self.send_error(400)
+            return
+
+        finally:            
+            self.set_header("Content-Type", "application/json")
+            self.write(json.dumps(str(response)))
+            self.finish()
+
+class SaveCountryHandler(tornado.web.RequestHandler) :
+    def initialize(self, url, handler) :
+        self.url = url
+        self.handler = handler
+    
+    @tornado.web.asynchronous
+    def post(self) :
+        try:
+            data = json.loads(self.request.body)
+            sessionToken = JSONHelper.getString(data, "session_token")
+            request = JSONHelper.getList(data, "request")
+            response = SaveCountry(sessionToken, request)
+        except Exception, e:
+            print e
+            self.send_error(400)
+            return
+        finally :
+            self.set_header("Content-Type", "application/json")
+            self.write(json.dumps(str(response)))
+            self.finish()
+
+class UpdateCountryHandler(tornado.web.RequestHandler) :
+    def initialize(self, url, handler) :
+        self.url = url
+        self.handler = handler
+
+    @tornado.web.asynchronous
+    def post(self) :
+        try:
+            data = json.loads(self.request.body)
+            sessionToken = JSONHelper.getString(data, "session_token")
+            request = JSONHelper.getList(data, "request")
+            response = UpdateCountry(sessionToken, request)
+
+        except Exception, e:
+            print e
+            self.send_error(400)
+            return
+        finally:
+            self.set_header("Content-Type", "application/json")
+            self.write(json.dumps(str(response)))
+            self.finish()
+
+class ChangeCountryStatusHandler(tornado.web.RequestHandler) :
+    def initialize(self, url, handler) :
+        self.url = url
+        self.handler = handler
+
+    @tornado.web.asynchronous
+    def post(self) :
+        try:
+            data = json.loads(self.request.body)
+            sessionToken = JSONHelper.getString(data, "session_token")
+            request = JSONHelper.getList(data, "request")
+            response = ChangeCountryStatus(sessionToken, request)
+
+        except Exception, e:
+            print e
+            self.send_error(400)
+            return
+        finally:
+            self.set_header("Content-Type", "application/json")
+            self.write(json.dumps(str(response)))
+            self.finish()
+
 def initializeKnowledgeHandler() :
     knowledge_urls = [
         ("/GetDomains", GetDomainHandler),
         ("/SaveDomain", SaveDomainHandler),
         ("/UpdateDomain", UpdateDomainHandler),
-        ("/ChangeDomainStatus", ChangeDomainStatusHandler)
+        ("/ChangeDomainStatus", ChangeDomainStatusHandler),
+
+        ("/GetCountries", GetCountryHandler),
+        ("/SaveCountry", SaveCountryHandler),
+        ("/UpdateCountry", UpdateCountryHandler),
+        ("/ChangeCountryStatus", ChangeCountryStatusHandler),
+
 
     ]
     return knowledge_urls
