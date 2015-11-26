2e4c5097b35e4fbd3a2fe0524e469fede74db06b
siva <siva@mindssoft.com>
2015-11-26 17:36:11 +0530

    update database url

--- Design/Src-server/aparajitha/server/knowledge/databasehandler.py
+++ Design/Src-server/aparajitha/server/knowledge/databasehandler.py
@@ -11,7 +11,7 @@ _databaseHandlerInstance = None
 
 class DatabaseHandler(object) :
     def __init__(self) :
-        self.mysqlHost = "192.168.1.2"
+        self.mysqlHost = "localhost"
         self.mysqlUser = "root"
         self.mysqlPassword = "123456"
         self.mysqlDatabase = "mirror_knowledge"
