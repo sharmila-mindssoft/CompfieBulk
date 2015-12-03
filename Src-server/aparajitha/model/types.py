76971a52aa706e0ae7c367c26c17d98779e3b679
dhanalakshmi <dhanalakshmisbl@mindssoft.com>
2015-12-03 12:16:27 +0530

    Src-server: Minor bug fix in OptionalType validation.

--- Src-server/aparajitha/model/types.py
+++ Src-server/aparajitha/model/types.py
@@ -57,7 +57,7 @@ class FloatType(object):
 		if (v < self.min_value) or (v > self.max_value) :
 			raise ValueError("value out of range")
 		return True
-		
+
 class UnsignedFloatType(object):
 	def __init__(self, bytes):
 		self.range = 2 ** (bytes * 8)
@@ -146,14 +146,26 @@ class DictType(object):
 		self.fields = fields
 
 	def validate(self, value):
+		def match_type(_type, _value) :
+			if not type(_type) in type_map[type(_value)] :
+				msg = "expecting type %s but received %s" % (_type, value[_key])
+				raise TypeError(msg)
+			return True
+
 		if not isinstance(value, dict) :
 			raise TypeError("%s not a dictionary" % (value,))
+
 		for _key, _type in self.fields.items():
 			if not _key in value.keys() :
 				raise ValueError("key: %s not found" % (_key,))
-			if not type(_type) in type_map[type(value[_key])] :
-				msg = "expecting type %s but received %s" % (_type, value[_key])
-				raise TypeError(msg)
+			if type(_type) == OptionalType :
+				if value[_key] is None :
+					continue
+				if match_type(_type._field_type(), value[_key]) :
+					continue
+			else :
+				if match_type(_type, value[_key]) :
+					continue
 		return True
 
 	def _keys(self):
