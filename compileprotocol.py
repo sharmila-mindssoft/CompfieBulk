36cd87ad7276e240cff994878d93d28678c58726
sharmila Madhavan <sharmila@mindssoft.com>
2015-12-25 12:30:49 +0530

    Added validation files

--- compileprotocol.py
+++
@@ -1,8 +0,0 @@
-from protocol import (
-	admin, clientadminsettings,
-	clientmasters, clientreport, clienttransactions,
-	clientuser, common, core, dashboard,
-	general, knowledgemaster, knowledgereport,
-	knowledgetransaction, login, technomasters,
-	technoreports, technotransactions
-)
