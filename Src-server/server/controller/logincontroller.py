3d8eb476155ecf15c6bdbd07b829dbdd86c39e1c
Usha Devi <usha@mindssoft.com>
2015-12-29 20:17:17 +0530

    logincontroller modified

--- Src-server/server/controller/logincontroller.py
+++ Src-server/server/controller/logincontroller.py
@@ -1,15 +1,22 @@
-from server.database import DatabaseHandler as db
 from protocol import login
 __all__ = [
 	"process_login"
 ]
 
 def process_login(request):
-	username = request.username
-	password = request.password
-	print username, password
-	return login.ResetPasswordSuccess()
+	pass
+	# username = request.username
+	# password = request.password
+	# print username, password
+	# return login.ResetPasswordSuccess()
 	# result = db.verify_login(username, password)
 	# print result
+	
 	# if result is True:
 
+
+def user_login_response(request):
+	pass
+
+def admin_login_response(request):
+	pass
