913b4ed1326099942eba981494c15cf8ca0b3536
Usha Devi <usha@mindssoft.com>
2016-01-13 15:00:14 +0530

    client transaction api added.

--- Src-server/server/clientmain.py
+++ Src-server/server/clientmain.py
@@ -122,6 +122,10 @@ class API(object):
     def handle_client_masters(self, request, db):
         return controller.process_client_master_requests(request, db)
 
+    @api_request(clienttransactions.RequestFormat)
+    def handle_client_transaction(self, request, db):
+        return controller.process_client_transaction_requests(request, db)
+
 template_loader = jinja2.FileSystemLoader(
     os.path.join(ROOT_PATH, "Src-client")
 )
@@ -203,6 +207,7 @@ def run_server(port):
         api_urls_and_handlers = [
             ("/api/login", api.handle_login),
             ("/api/client_masters", api.handle_client_masters),
+            ("/api/client_transaction", api.handle_client_transaction),
         ]
         for url, handler in api_urls_and_handlers:
             web_server.url(url, POST=handler, OPTIONS=cors_handler)
