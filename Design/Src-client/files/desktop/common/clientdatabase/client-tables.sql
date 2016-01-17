cc54fb27a4cc7fe4ed1c02db68405aa5decf2ce1
Usha Devi <usha@mindssoft.com>
2016-01-17 17:06:08 +0530

    primary key issue bug fixed.

--- Design/Src-client/files/desktop/common/clientdatabase/client-tables.sql
+++ Design/Src-client/files/desktop/common/clientdatabase/client-tables.sql
@@ -263,8 +263,6 @@ DROP TABLE IF EXISTS `tbl_client_compliances`;
 CREATE TABLE `tbl_client_compliances` (
   `client_statutory_id` int(11) NOT NULL,
   `compliance_id` int(11) NOT NULL,
-  `unit_id` int(11) NOT NULL,
-  `statutory_provision` VARCHAR(50) NOT NULL,
   `applicable` tinyint(4) DEFAULT NULL,
   `not_applicable_remarks` varchar(250) DEFAULT NULL,
   `compliance_applicable` tinyint(4) DEFAULT NULL,
@@ -274,8 +272,7 @@ CREATE TABLE `tbl_client_compliances` (
   `created_by` int(11) NOT NULL,
   `created_on` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_by` int(11) NOT NULL,
-  `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`compliance_id`, `unit_id`)
+  `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 DROP TABLE IF EXISTS `tbl_assigned_compliances`;
 CREATE TABLE `tbl_assigned_compliances` (
@@ -297,7 +294,6 @@ CREATE TABLE `tbl_assigned_compliances` (
   `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   CONSTRAINT `fk_assigned_compliances_countries` FOREIGN KEY (`country_id`) REFERENCES `tbl_countries` (`country_id`),
   CONSTRAINT `fk_assigned_compliances_units` FOREIGN KEY (`unit_id`) REFERENCES `tbl_units` (`unit_id`),
-  CONSTRAINT `fk_assigned_compliances_compliances` FOREIGN KEY (`compliance_id`) REFERENCES `tbl_client_compliances` (`compliance_id`),
   CONSTRAINT `fk_assigned_compliances_assignee_client_user_details` FOREIGN KEY (`assignee`) REFERENCES `tbl_users` (`user_id`),
   CONSTRAINT `fk_assigned_compliances_concurrence_client_user_details` FOREIGN KEY (`concurrence_person`) REFERENCES `tbl_users` (`user_id`),
   CONSTRAINT `fk_assigned_compliances_approve_client_user_details` FOREIGN KEY (`approval_person`) REFERENCES `tbl_users` (`user_id`)
@@ -316,7 +312,6 @@ CREATE TABLE `tbl_reassigned_compliances_history` (
   `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   CONSTRAINT `fk_rch_assignee_user_details` FOREIGN KEY (`assignee`) REFERENCES `tbl_users` (`user_id`),
   CONSTRAINT `fk_rch_reassigned_from_user_details` FOREIGN KEY (`reassigned_from`) REFERENCES `tbl_users` (`user_id`),
-  CONSTRAINT `fk_rch_compliances` FOREIGN KEY (`compliance_id`) REFERENCES `tbl_client_compliances` (`compliance_id`),
   CONSTRAINT `fk_rch_units` FOREIGN KEY (`unit_id`) REFERENCES `tbl_units` (`unit_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 DROP TABLE IF EXISTS `tbl_compliance_status`;
