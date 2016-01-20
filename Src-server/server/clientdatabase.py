d9d1799bccf9cd2663502917133609538b6eb02a
Usha Devi <usha@mindssoft.com>
2016-01-20 15:13:05 +0530

    bug fixed.

--- Src-server/server/clientdatabase.py
+++ Src-server/server/clientdatabase.py
@@ -37,7 +37,7 @@ class ClientDatabase(Database):
             self.begin()
             self._client_db_connections[int(client_id)] = self._connection
             self._client_db_cursors[int(client_id)] = self._cursor
-        #print self._client_db_cursors
+        print self._client_db_cursors
         self.initialize_table_names()
 
     def execute(self, query, client_id = None) :
@@ -171,6 +171,7 @@ class ClientDatabase(Database):
         columns = "client_id"
         condition = "url_short_name = '%s'"% short_name
         rows = self.get_data("tbl_client_groups", columns, condition, 0)
+        print rows
         return rows[0][0]
 
     def verify_username(self, username, client_id):
@@ -312,7 +313,9 @@ class ClientDatabase(Database):
         condition = "1"
         if business_group_ids != None:
             condition = "business_group_id in (%s)" % business_group_ids
+        print condition
         rows = self.get_data(self.tblBusinessGroups, columns, condition, client_id) 
+        print rows
         columns = ["business_group_id", "business_group_name"]
         result = self.convert_to_dict(rows, columns)
         return self.return_business_groups(result)
