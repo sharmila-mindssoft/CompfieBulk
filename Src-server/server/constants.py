2c89421bda761f4186476308abb86947c65fd601
usha <usha@mindssoft.com>
2016-03-13 10:29:58 +0530

    version added.

--- Src-server/server/constants.py
+++ Src-server/server/constants.py
@@ -367,7 +367,7 @@ TEMPLATE_PATHS = [
 
 ]
 
-VERSION = 1
+VERSION = 2
 
 KNOWLEDGE_DB_HOST = "localhost"
 KNOWLEDGE_DB_PORT = 3306
@@ -388,7 +388,7 @@ KNOWLEDGE_URL = "http://localhost:8082/knowledge"
 # CLIENT_URL = "http://52.11.242.90:8082/"
 # KNOWLEDGE_URL = "http://52.11.242.90:8080/knowledge"
 
-# # aparajitha server
+# # # aparajitha server
 # KNOWLEDGE_DB_HOST = "localhost"
 # KNOWLEDGE_DB_PORT = 3306
 # KNOWLEDGE_DB_USERNAME = "root"
