b019656aab7b60b5e89da564568c419d8a7877f9
usha <usha@mindssoft.com>
2016-03-16 17:51:51 +0530

    logger updated.

--- Src-server/server/logger.py
+++ Src-server/server/logger.py
@@ -4,31 +4,36 @@ ROOTPATH = ""
 knowledge_log_path = "logs/knowledge/knowledge-log"
 client_log_path = "logs/client/client-log"
 
-log_format = logging.Formatter("%(asctime)s - %(name)s - %(message)s")
+knowledge_log_format = logging.Formatter("%(asctime)s - %(name)s - %(message)s")
+
 
 rotateFileHandler = logging.handlers.TimedRotatingFileHandler(
     knowledge_log_path,
     when="midnight",
-    backupCount=10
+    backupCount=1
 )
-rotateFileHandler.setFormatter(log_format)
+rotateFileHandler.suffix = "%Y-%m-%d"
+rotateFileHandler.setFormatter(knowledge_log_format)
 
 knowledgeLogger = logging.getLogger("knowledge_logger")
 knowledgeLogger.setLevel(logging.DEBUG)
 knowledgeLogger.addHandler(rotateFileHandler)
 
+client_log_format = logging.Formatter("%(asctime)s - %(name)s - %(message)s")
 cRotateFileHandler = logging.handlers.TimedRotatingFileHandler(
     client_log_path,
     when="midnight",
-    backupCount=10
+    backupCount=1
 )
-cRotateFileHandler.setFormatter(log_format)
+cRotateFileHandler.suffix = "%Y-%m-%d"
+cRotateFileHandler.setFormatter(client_log_format)
 
 clientLogger = logging.getLogger("client_logger")
 clientLogger.setLevel(logging.DEBUG)
 clientLogger.addHandler(cRotateFileHandler)
 
 def logKnowledge(log_level, from_file_name, message) :
+    print "logKnowledge"
     log_message = "%s : %s" % (from_file_name, message)
     if log_level == "debug" :
         knowledgeLogger.debug(log_message)
