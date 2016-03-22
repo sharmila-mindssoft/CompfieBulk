0524b02f85b19e286554971195dfeff096bae861
usha <usha@mindssoft.com>
2016-03-22 16:00:19 +0530

    compliance_status chart updated.

--- Src-server/server/clientdatabase.py
+++ Src-server/server/clientdatabase.py
@@ -3294,7 +3294,7 @@ class ClientDatabase(Database):
             %s \
             %s \
             %s \
-            ORDER BY T1.due_date desc" % (
+            ORDER BY T1.due_date" % (
                 user_qry,
                 str(tuple(domain_ids)),
                 date_qry,
@@ -3370,7 +3370,7 @@ class ClientDatabase(Database):
         return self.return_compliance_details_drill_down(year_info, compliance_status, request.year, result, client_id)
 
     def return_compliance_details_drill_down(self, year_info, compliance_status, request_year, result, client_id) :
-        current_date = datetime.date.today()
+        current_date = datetime.datetime.today()
 
         unit_wise_data = {}
         for r in result :
@@ -3665,7 +3665,7 @@ class ClientDatabase(Database):
             "start_date", "due_date"
         ]
         not_complied = self.convert_to_dict(rows, columns)
-        current_date = datetime.date.today()
+        current_date = datetime.datetime.today()
         below_30 = 0
         below_60 = 0
         below_90 = 0
@@ -3724,7 +3724,7 @@ class ClientDatabase(Database):
             domain_ids, date_qry, not_complied_status_qry,
             filter_type_qry, session_user
         )
-        current_date = datetime.date.today()
+        current_date = datetime.datetime.today()
         not_complied_details_filtered = []
 
         for c in not_complied_details :
@@ -3744,7 +3744,6 @@ class ClientDatabase(Database):
                 if ageing > 90 :
                     not_complied_details_filtered.append(c)
 
-        current_date = datetime.date.today()
 
         unit_wise_data = {}
         for r in not_complied_details_filtered :
