2428fc0f5286c685b5234c002eec40ff8a9c90a3
usha <usha@mindssoft.com>
2016-04-06 12:48:30 +0530

    trigger added for client mobile sync

--- Src-server/server/scripts/client_trigger_script.sql
+++ Src-server/server/scripts/client_trigger_script.sql
@@ -17,3 +17,75 @@ CREATE TRIGGER `after_tbl_statutory_notifications_units_insert` AFTER INSERT ON 
 END
 //
 DELIMITER ;
+
+DROP TRIGGER IF EXISTS `after_tbl_units_insert`;
+DELIMITER //
+CREATE TRIGGER `after_tbl_units_insert` AFTER INSERT ON `tbl_units`
+FOR EACH ROW BEGIN
+	CALL procedure_to_update_version("unit");
+END
+//
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS `after_tbl_units_update`;
+DELIMITER //
+CREATE TRIGGER `after_tbl_units_update` AFTER UPDATE ON `tbl_units`
+FOR EACH ROW BEGIN
+	CALL procedure_to_update_version("unit");
+END
+//
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS `after_tbl_users_update`;
+DELIMITER //
+CREATE TRIGGER `after_tbl_users_update` AFTER UPDATE ON `tbl_users`
+FOR EACH ROW BEGIN
+	CALL procedure_to_update_version("user");
+END
+//
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS `after_tbl_users_insert`;
+DELIMITER //
+CREATE TRIGGER `after_tbl_users_insert` AFTER INSERT ON `tbl_users`
+FOR EACH ROW BEGIN
+	CALL procedure_to_update_version("user");
+END
+//
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS `after_tbl_compliance_history_insert`;
+DELIMITER //
+CREATE TRIGGER `after_tbl_compliance_history_insert` AFTER INSERT ON `tbl_compliance_history`
+FOR EACH ROW BEGIN
+	CALL procedure_to_update_version("history");
+END
+//
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS `after_tbl_client_compliances_insert`;
+DELIMITER //
+CREATE TRIGGER `after_tbl_client_compliances_insert` AFTER INSERT ON `tbl_client_compliances`
+FOR EACH ROW BEGIN
+	CALL procedure_to_update_version("compliance");
+END
+//
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS `after_tbl_client_compliances_update`;
+DELIMITER //
+CREATE TRIGGER `after_tbl_client_compliances_update` AFTER UPDATE ON `tbl_client_compliances`
+FOR EACH ROW BEGIN
+	CALL procedure_to_update_version("compliance");
+END
+//
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS `after_tbl_client_compliances_update`;
+DELIMITER //
+CREATE TRIGGER `after_tbl_client_compliances_update` AFTER UPDATE ON `tbl_client_compliances`
+FOR EACH ROW BEGIN
+	CALL procedure_to_update_version("compliance");
+END
+//
+DELIMITER ;
