c1dc5d5beca239ac4f048451ae5b64bb962028a6
sharmila-mindssoft <sharmila@mindssoft.com>
2016-05-14 18:23:41 +0530

    Restricted html file upload in compliance task details

--- Src-server/server/clientcontroller/clientusercontroller.py
+++ Src-server/server/clientcontroller/clientusercontroller.py
@@ -86,18 +86,31 @@ def process_get_upcoming_compliance_detail(db, request, session_user, client_id)
 ########################################################
 # To validate and update the compliance details
 ########################################################
+def is_unsupported_file(documents):
+    for doc in documents:
+        file_name_parts = doc.file_name.split('.')
+        name = doc.file_name.split('.')[0]
+        exten = doc.file_name.split('.')[1]
+        if exten in ["exe", "htm", "html", "xhtml"]:
+            return True
+        else:
+            continue
+
 def process_update_compliance_detail(db, request, session_user, client_id):
-    result = db.update_compliances(
-        request.compliance_history_id, request.documents,
-        request.completion_date, request.validity_date, request.next_due_date,
-        request.remarks, client_id, session_user
-    )
-    if result is True:
-        return clientuser.UpdateComplianceDetailSuccess()
-    elif result == "InvalidUser":
-        return clientuser.InvalidUser()
+    if is_unsupported_file(request.documents):
+        return clientuser.UnSupportedFile()
     else:
-        return clientuser.NextDueDateMustBeWithIn90DaysBeforeValidityDate()
+        result = db.update_compliances(
+            request.compliance_history_id, request.documents,
+            request.completion_date, request.validity_date, request.next_due_date,
+            request.remarks, client_id, session_user
+        )
+        if result is True:
+            return clientuser.UpdateComplianceDetailSuccess()
+        elif result == "InvalidUser":
+            return clientuser.InvalidUser()
+        else:
+            return clientuser.NextDueDateMustBeWithIn90DaysBeforeValidityDate()
 
 ########################################################
 # To get the list of all on occurrence compliances
