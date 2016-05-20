0fc32b07399af8205084f33e74f1f1de221e56e4
sharmila-mindssoft <sharmila@mindssoft.com>
2016-05-20 07:30:58 +0530

    fixed (combined queires) approval, reject, concurrence functionalities

--- Src-server/server/database.py
+++ Src-server/server/database.py
@@ -306,6 +306,7 @@ class Database(object) :
             else:
                 query += column+" = '"+str(values[index])+"' "
         query += " WHERE "+condition
+        print query
         try:
             return self.execute(query)
         except Exception, e:
