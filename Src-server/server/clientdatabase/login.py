72610f783080e80a0036ac30f7d23f6ea5101729
sharmila-mindssoft <sharmila@mindssoft.com>
2016-08-12 14:21:05 +0530

    Fixed bug while loging in as service provider user

--- Src-server/server/clientdatabase/login.py
+++ Src-server/server/clientdatabase/login.py
@@ -5,7 +5,7 @@ from server.common import (
 )
 
 from server.clientdatabase.general import (
-    is_service_proivder_user
+    is_service_proivder_user, is_service_provider_in_contract
 )
 from server.clientdatabase.savetoknowledge import IsClientActive
 
@@ -21,6 +21,7 @@ __all__ = [
 
     ]
 
+
 def is_configured(db):
     columns = "count(1) as configure"
     condition = "1"
@@ -32,9 +33,11 @@ def is_configured(db):
     else:
         return True
 
+
 def is_in_contract(db):
     columns = "count(1)  as contract"
-    condition = "now() between contract_from and DATE_ADD(contract_to, INTERVAL 1 DAY)"
+    condition = "now() between contract_from and " + \
+        " DATE_ADD(contract_to, INTERVAL 1 DAY)"
     rows = db.get_data(
         tblClientGroups, columns, condition
     )
@@ -42,6 +45,7 @@ def is_in_contract(db):
         return False
     else:
         return True
+
 
 def is_contract_not_started(db):
     columns = "count(1) as contract"
@@ -54,9 +58,11 @@ def is_contract_not_started(db):
     else:
         return True
 
+
 def is_client_active(client_id):
     t_obj = IsClientActive(client_id)
     return t_obj._is_client_active()
+
 
 def verify_login(db, username, password):
     data_columns = [
@@ -65,21 +71,21 @@ def verify_login(db, username, password):
         "user_group_name", "form_ids", "is_admin",
         "service_provider_id", "is_primary_admin"
     ]
-    query = "SELECT t1.user_id, t1.user_group_id, t1.email_id, \
-        t1.employee_name, t1.employee_code, t1.contact_no, \
-        t2.user_group_name, t2.form_ids, t1.is_admin, \
-        t1.service_provider_id, t1.is_primary_admin \
-        FROM tbl_users t1 LEFT JOIN tbl_user_groups t2\
-        ON t1.user_group_id = t2.user_group_id \
-        WHERE t1.password= %s and t1.email_id= %s and t1.is_active=1"
+    query = "SELECT t1.user_id, t1.user_group_id, t1.email_id, " + \
+        " t1.employee_name, t1.employee_code, t1.contact_no, " + \
+        " t2.user_group_name, t2.form_ids, t1.is_admin, " + \
+        " t1.service_provider_id, t1.is_primary_admin " + \
+        " FROM tbl_users t1 LEFT JOIN tbl_user_groups t2 " + \
+        " ON t1.user_group_id = t2.user_group_id " + \
+        " WHERE t1.password= %s and t1.email_id= %s and t1.is_active=1"
 
     data_list = db.select_one(query, [password, username])
     print data_list
-    if data_list is None :
+    if data_list is None:
         return False
-    else :
+    else:
         result = convert_to_dict(data_list, data_columns)
-        if result["is_primary_admin"] == 1 :
+        if result["is_primary_admin"] == 1:
             return True
         if is_service_proivder_user(db, result["user_id"]):
             if (
@@ -94,10 +100,11 @@ def verify_login(db, username, password):
         else:
             return result
 
+
 def add_session(
     db, user_id, session_type_id, ip,
     employee, client_id=None
-) :
+):
     if client_id is not None:
         clear_old_session(db, user_id, session_type_id, client_id)
     else:
@@ -106,9 +113,9 @@ def add_session(
     if client_id is not None:
         session_id = "%s-%s" % (client_id, session_id)
     updated_on = get_date_time()
-    query = "INSERT INTO tbl_user_sessions \
-        (session_token, user_id, session_type_id, last_accessed_time) \
-        VALUES (%s, %s, %s, %s);"
+    query = "INSERT INTO tbl_user_sessions " + \
+        " (session_token, user_id, session_type_id, last_accessed_time) " + \
+        " VALUES (%s, %s, %s, %s);"
     db.execute(query, (session_id, user_id, session_type_id, updated_on))
 
     # action = "Log In by - \"%s\" from \"%s\"" % ( employee, ip)
@@ -117,26 +124,30 @@ def add_session(
 
     return session_id
 
+
 #
 # mobile_api
 #
-
 def get_client_group(db):
     q = "SELECT client_id, group_name from  tbl_client_groups"
     row = db.select_one(q)
     result = []
-    if row :
+    if row:
         result = convert_to_dict(row, ["client_id", "group_name"])
     return result
 
+
 def get_client_configuration(db):
-    q = "SELECT country_id, domain_id, period_from, period_to from tbl_client_configurations"
+    q = "SELECT country_id, domain_id, period_from, period_to " + \
+        " from tbl_client_configurations"
     rows = db.select_all(q)
     result = []
-    if rows :
-        result = convert_to_dict(rows, ["country_id", "domain_id", "period_from", "period_to"])
+    if rows:
+        result = convert_to_dict(
+            rows, ["country_id", "domain_id", "period_from", "period_to"]
+        )
     c_list = []
-    for r in result :
+    for r in result:
         info = core.ClientConfiguration(
             r["country_id"],
             r["domain_id"],
@@ -147,10 +158,10 @@ def get_client_configuration(db):
 
     return c_list
 
-def clear_old_session(db, user_id, session_type_id, client_id=None) :
-    query = "DELETE FROM tbl_user_sessions \
-        WHERE user_id=%s and session_type_id=%s"
 
+def clear_old_session(db, user_id, session_type_id, client_id=None):
+    query = "DELETE FROM tbl_user_sessions " + \
+        " WHERE user_id=%s and session_type_id=%s"
     db.execute(query, (
         user_id, session_type_id
     ))
