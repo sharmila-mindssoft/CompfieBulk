fafc0e391cb749129496def6f081a4e00b004a9e
usha <usha@mindssoft.com>
2016-09-20 10:18:25 +0530

    Definer removed from procedure script.

--- Src-server/server/scripts/knowledge_procedures.sql
+++ Src-server/server/scripts/knowledge_procedures.sql
@@ -3,12 +3,12 @@
 -- --------------------------------------------------------------------------------
 DELIMITER $$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_country_mapped_list`()
+CREATE PROCEDURE `sp_country_mapped_list`()
 BEGIN
 	SELECT country_id, country_name, is_active
 	FROM tbl_countries
 	WHERE country_id IN (
-		SELECT DISTINCT country_id 
+		SELECT DISTINCT country_id
 		FROM tbl_statutory_levels
 	);
 END
@@ -18,7 +18,7 @@ END
 -- --------------------------------------------------------------------------------
 DELIMITER $$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_domain_mapped_list`()
+CREATE PROCEDURE `sp_domain_mapped_list`()
 BEGIN
 	SELECT domain_id, domain_name, is_active
 	FROM tbl_domains
@@ -33,21 +33,21 @@ END
 -- --------------------------------------------------------------------------------
 DELIMITER $$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_validitydays_settings_list`()
+CREATE PROCEDURE `sp_validitydays_settings_list`()
 BEGIN
 	SELECT validity_days_id, country_id, domain_id, days
 	FROM tbl_validity_days_settings;
 END
 
 -- --------------------------------------------------------------------------------
--- Returns all possible country domain combinations which are mapped during 
+-- Returns all possible country domain combinations which are mapped during
 -- Statutory level creation
 -- --------------------------------------------------------------------------------
 DELIMITER $$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_statutorylevels_mappings`()
+CREATE PROCEDURE `sp_statutorylevels_mappings`()
 BEGIN
-	SELECT country_id, domain_id 
+	SELECT country_id, domain_id
 	FROM tbl_statutory_levels;
 END
 
@@ -56,21 +56,21 @@ END
 -- --------------------------------------------------------------------------------
 DELIMITER $$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_validitydays_settings_save`(
+CREATE PROCEDURE `sp_validitydays_settings_save`(
 	IN validitydaysid INT(11), countryid INT(11), domainid INT(11),
-	validitydays INT(11), createdby INT(11), 
+	validitydays INT(11), createdby INT(11),
 	createdon DATETIME, updatedby INT(11),
 	updatedon DATETIME
 )
 BEGIN
 	IF validitydaysid is null then
-		INSERT INTO tbl_validity_days_settings 
-		(country_id, domain_id, days, created_by, created_on, updated_by, updated_on) 
-		VALUES 
+		INSERT INTO tbl_validity_days_settings
+		(country_id, domain_id, days, created_by, created_on, updated_by, updated_on)
+		VALUES
 		(countryid, domainid, validitydays, createdby, createdon, updatedby, updatedon);
 	ELSE
 		Update tbl_validity_days_settings set days=validitydays,
-		updated_on = updatedon, updated_by = updatedby 
+		updated_on = updatedon, updated_by = updatedby
 		WHERE validity_days_id = validitydaysid;
 	END IF;
 END
@@ -79,7 +79,7 @@ END
 -- To check duplicate industry name
 -- --------------------------------------------------------------------------------
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_industry_master_checkduplicateindustry`(
+CREATE PROCEDURE `sp_industry_master_checkduplicateindustry`(
 in industryid int(11), industryname varchar(50))
 BEGIN
 	if industryid = 0 then
@@ -95,7 +95,7 @@ END
 -- To get industry name by its id
 -- --------------------------------------------------------------------------------
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_industry_master_getindusdtrybyid`(industryid int(11))
+CREATE PROCEDURE `sp_industry_master_getindusdtrybyid`(industryid int(11))
 BEGIN
 	SELECT industry_name FROM tbl_industries WHERE
     industry_id = industryid;
@@ -106,7 +106,7 @@ END
 -- To get industry details from its master
 -- --------------------------------------------------------------------------------
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_industry_master_getindustries`()
+CREATE PROCEDURE `sp_industry_master_getindustries`()
 BEGIN
 	SELECT t1.country_id, t2.country_name, t1.domain_id, t3.domain_name,
 		t1.industry_id, t1.industry_name, t1.is_active FROM tbl_industries t1
@@ -118,7 +118,7 @@ END
 -- To save industry activity log in activity log table
 -- --------------------------------------------------------------------------------
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_industry_master_saveactivitylog`(
+CREATE PROCEDURE `sp_industry_master_saveactivitylog`(
 activityLogId int(11), userId int(11), formId int(11),
 action varchar(500), createdOn timestamp)
 BEGIN
@@ -132,7 +132,7 @@ END
 -- To save industry master details
 -- --------------------------------------------------------------------------------
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_industry_master_saveindustry`(
+CREATE PROCEDURE `sp_industry_master_saveindustry`(
 countryid int(11), domainid int(11), industryname varchar(50),
 createdby int(11), createdon timestamp)
 BEGIN
@@ -146,7 +146,7 @@ END
 -- To update industry master details
 -- --------------------------------------------------------------------------------
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_industry_master_updateindustry`(
+CREATE PROCEDURE `sp_industry_master_updateindustry`(
 	industryId int(11), industryName varchar(50), countryId int(11),
     domainId int(11), updatedBy int(11)
 )
@@ -165,7 +165,7 @@ END
 -- To update industry master status
 -- --------------------------------------------------------------------------------
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_industry_master_updatestatus`(
+CREATE PROCEDURE `sp_industry_master_updatestatus`(
 	industryId int(11), isActive tinyint(4), updatedBy int(11))
 BEGIN
 	UPDATE tbl_industries
@@ -180,7 +180,7 @@ END
 -- To check for dupliacte statutory nature name
 -- --------------------------------------------------------------------------------
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_statutory_nature_checkduplicatenature`(
+CREATE PROCEDURE `sp_statutory_nature_checkduplicatenature`(
 	statutoryNatureName varchar(50),
     statutoryNatureId int(11)
 )
@@ -199,7 +199,7 @@ END
 -- To -get statutory nature master details
 -- --------------------------------------------------------------------------------
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_statutory_nature_getstatutorynatures`()
+CREATE PROCEDURE `sp_statutory_nature_getstatutorynatures`()
 BEGIN
 	SELECT t1.statutory_nature_id, t1.statutory_nature_name,
     t1.country_id, t2.country_name, t1.is_active
@@ -215,7 +215,7 @@ END
 -- To update statutory nature master details
 -- --------------------------------------------------------------------------------
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_statutory_nature_updatestatutorynature`(
+CREATE PROCEDURE `sp_statutory_nature_updatestatutorynature`(
 IN statutoryNatureId int(11), statutoryNatureName varchAR(50),
 countryId int(11), updatedBy int(11))
 BEGIN
@@ -231,7 +231,7 @@ END
 -- To update statutory nature status
 -- --------------------------------------------------------------------------------
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_statutory_nature_updatestatutorynaturestatus`(
+CREATE PROCEDURE `sp_statutory_nature_updatestatutorynaturestatus`(
 statutoryNatureId int(11), updatedBy int(11), isActive tinyint(4))
 BEGIN
 	update tbl_statutory_natures set
@@ -245,7 +245,7 @@ END
 -- To get statutory nature details by its id
 -- --------------------------------------------------------------------------------
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_statutory_natures_getnaturebyid`(
+CREATE PROCEDURE `sp_statutory_natures_getnaturebyid`(
 	statutoryNatureId int(11))
 BEGIN
 	SELECT statutory_nature_name FROM tbl_statutory_natures
@@ -257,7 +257,7 @@ END
 -- To save statutory nature details
 -- --------------------------------------------------------------------------------
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_statutorynature_savestatutorynature`(
+CREATE PROCEDURE `sp_statutorynature_savestatutorynature`(
 statutoryNatureName varchar(50), countryId int(11),
 createdBy int(11), createdOn timestamp)
 BEGIN
