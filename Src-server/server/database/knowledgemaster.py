e47dd99ba0a200f7760569a30a4b7cf88bdafaf0
Jegan <jegan@mindssoft.com>
2016-09-22 15:40:00 +0530

    modfication in def check dupliacate industry

--- Src-server/server/database/knowledgemaster.py
+++ Src-server/server/database/knowledgemaster.py
@@ -102,6 +102,8 @@ def check_duplicate_industry(db, industry_name, industry_id):
 
     row = db.call_proc("sp_industry_master_checkduplicateindustry", param)
     for r in row:
+        print "row"
+        print r[0]
         if r[0] > 0:
             isDuplicate = True
     return isDuplicate
