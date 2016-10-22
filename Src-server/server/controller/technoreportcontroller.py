5cb77c42d7ce5ab5decd2a1f376702d071233e23
usha <usha@mindssoft.com>
2016-10-22 16:50:10 +0530

    function created for user mapping report functionality - search filter, report data

--- Src-server/server/controller/technoreportcontroller.py
+++ Src-server/server/controller/technoreportcontroller.py
@@ -1,5 +1,5 @@
 import time
-from protocol import login, technoreports, knowledgereport
+from protocol import login, technoreports, knowledgereport, core
 from generalcontroller import validate_user_session, validate_user_forms
 from server import logger
 from server.constants import RECORD_DISPLAY_COUNT
@@ -25,7 +25,14 @@ from server.database.technoreport import (
     get_statutory_notifications_report_data,
     get_client_details_report,
     get_client_details_report_count,
-    get_compliance_list_report_techno
+    get_compliance_list_report_techno,
+    get_user_category_details,
+    get_countries_for_usermapping_report_filter,
+    get_group_details_for_usermapping_report_filter,
+    get_business_groups_for_usermapping_report,
+    get_legal_entities_for_usermapping_report,
+    get_unit_details_for_usermapping_report,
+    get_usermapping_report_dataset
 )
 
 __all__ = [
@@ -38,6 +45,8 @@ forms = [22, 23, 24, 25]
 def process_techno_report_request(request, db):
     session_token = request.session_token
     request_frame = request.request
+    print "request_frame"
+    print request_frame
     user_id = validate_user_session(db, session_token)
     if user_id is not None:
         is_valid = validate_user_forms(db, user_id, forms, request_frame)
@@ -134,6 +143,12 @@ def process_techno_report_request(request, db):
         logger.logKnowledgeApi("------", str(time.time()))
         result = process_get_user_mapping_reports_filter(db, request_frame, user_id)
         logger.logKnowledgeApi("GetUserMappingReportFilters", "process end")
+        logger.logKnowledgeApi("------", str(time.time()))
+    elif type(request_frame) is technoreports.GetUserMappingDetailsReportData:
+        logger.logKnowledgeApi("GetUserMappingDetailsReportData", "process begin")
+        logger.logKnowledgeApi("------", str(time.time()))
+        result = process_get_user_mapping_details_reports_data(db, request_frame, user_id)
+        logger.logKnowledgeApi("GetUserMappingDetailsReportData", "process end")
         logger.logKnowledgeApi("------", str(time.time()))
 
     return result
@@ -250,15 +265,74 @@ def process_get_compliance_task_report(db, request_frame, user_id):
     )
 
 def process_get_user_mapping_reports_filter(db, request_frame, session_user):
+    print "inside user mapping report controller"
     user_category_details = get_user_category_details(db, session_user)
-
+    print "user category details"
+    print user_category_details
     for row in user_category_details:
-        countries = get_countries_for_usermapping_report_filter(db, (int(row["user_category_id"]), int(session_user)))
-        usermapping_group_details = get_group_details_for_usermapping_report_filter(db, (int(row["user_category_id"]), int(session_user)))
+        countries = get_countries_for_usermapping_report_filter(db, int(row["user_category_id"]), int(session_user))
+        usermapping_groupdetails = get_group_details_for_usermapping_report_filter(db, int(row["user_category_id"]), int(session_user))
         usermapping_business_groups = get_business_groups_for_usermapping_report(db)
         usermapping_legal_entities = get_legal_entities_for_usermapping_report(db)
-        usermapping_unit = get_unit_details_for_usermapping_report(db, (int(row["user_category_id"]), int(session_user)))
-        return GetUserMappingReportFiltersSuccess(
-            countries = countries, usermapping_group_details = usermapping_group_details, usermapping_business_groups = usermapping_business_groups,
-            usermapping_legal_entities = usermapping_legal_entities, usermapping_unit = usermapping_unit
+        usermapping_unit = get_unit_details_for_usermapping_report(db, int(row["user_category_id"]), int(session_user))
+        return technoreports.GetUserMappingReportFiltersSuccess(
+            countries = countries,
+            usermapping_groupdetails = usermapping_groupdetails,
+            usermapping_business_groups = usermapping_business_groups,
+            usermapping_legal_entities = usermapping_legal_entities,
+            usermapping_unit = usermapping_unit
+        )
+
+def process_get_user_mapping_details_reports_data(db, request_frame, session_user):
+    print "inside user mapping report details"
+    country_id = request_frame.country_id
+    client_id = request_frame.client_id
+    legal_entity_id = request_frame.legal_entity_id
+    usermapping_report_dataset = []
+    usermapping_report_dataset = get_usermapping_report_dataset(db, int(session_user), client_id, legal_entity_id, country_id)
+    print "length of mapping after db"
+    print usermapping_report_dataset
+    techno_details = []
+    unit_domains = []
+    domains = []
+    if(len(usermapping_report_dataset) > 0):
+        print "ds 1"
+        print usermapping_report_dataset[1]
+
+        for techno in usermapping_report_dataset[0]:
+            techno_details.append(core.UserMappingReportTechno(
+                techno["unit_id"], techno["techno_manager"], techno["techno_user"]
+            ))
+        print "techno"
+        print techno_details
+
+        for assign_domain in usermapping_report_dataset[1]:
+            unit_domains.append(core.UserMappingReportDomain(
+                assign_domain["unit_id"], assign_domain["employee_name"], assign_domain["user_category_name"], assign_domain["domain_id"]
+            ))
+        print "unit_domains"
+        print unit_domains
+        for domain in usermapping_report_dataset[2]:
+            domains.append(core.Domain(
+                domain["domain_id"], domain["domain_name"], bool(domain["is_active"])
+            ))
+        '''if(len(usermapping_report_dataset[0]) > 0):
+            techno_details = usermapping_report_dataset[0]
+        else:
+            techno_details = None,
+
+        if(len(usermapping_report_dataset[1]) > 0):
+            unit_domains = usermapping_report_dataset[1]
+        else:
+            unit_domains = None,
+
+        if(len(usermapping_report_dataset[2]) > 0):
+            domains = usermapping_report_dataset[2]
+        else:
+            domains = None'''
+
+        return technoreports.GetUserMappingReportDataSuccess(
+            techno_details = techno_details,
+            unit_domains = unit_domains,
+            domains = domains
         )
