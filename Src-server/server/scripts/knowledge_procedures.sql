47c5c9550e70e308f4d04ff4939133d6d4979cc6
usha <usha@mindssoft.com>
2016-11-25 18:52:39 +0530

    included procedure for geography master report

--- Src-server/server/scripts/knowledge_procedures.sql
+++ Src-server/server/scripts/knowledge_procedures.sql
@@ -4922,3 +4922,51 @@ BEGIN
 	legal_entity_id = _le_id;
 END//
 DELIMITER;
+
+-- --------------------------------------------------------------------------------
+-- Routine DDL
+-- Note: comments before and after the routine body will not be stored by the server
+-- --------------------------------------------------------------------------------
+DROP PROCEDURE IF EXISTS `sp_geographymaster_report_data`;
+DELIMITER //
+
+CREATE PROCEDURE `sp_geographymaster_report_data`()
+BEGIN
+	SELECT t1.geography_id, t1.geography_name, t1.parent_names, t1.is_active,
+	(select distinct country_id FROM tbl_geography_levels
+	where level_id = t1.level_id) country_id,
+         (select level_position FROM tbl_geography_levels
+         where level_id = t1.level_id) position
+         FROM tbl_geographies t1
+         ORDER BY position, parent_names, geography_name;
+END//
+DELIMITER;
+
+-- --------------------------------------------------------------------------------
+-- Routine DDL
+-- Note: comments before and after the routine body will not be stored by the server
+-- --------------------------------------------------------------------------------
+DROP PROCEDURE IF EXISTS `sp_geographymaster_geographies_list`;
+DELIMITER //
+
+CREATE PROCEDURE `sp_geographymaster_geographies_list`(
+in countryId int(11))
+BEGIN
+	if countryId is not null then
+		SELECT distinct t1.geography_id, t1.geography_name, t1.level_id,
+		t1.parent_ids, t1.is_active, t2.country_id,
+			(select country_name from tbl_countries where country_id = t2.country_id)
+			as country_name, t2.level_position, t1.parent_names FROM tbl_geographies t1
+			INNER JOIN tbl_geography_levels t2 on t1.level_id = t2.level_id
+			AND t2.country_id=countryId
+			ORDER BY country_name, level_position, geography_name;
+	else
+		SELECT distinct t1.geography_id, t1.geography_name, t1.level_id,
+		t1.parent_ids, t1.is_active, t2.country_id,
+			(select country_name from tbl_countries where country_id = t2.country_id)
+			as country_name, t2.level_position, t1.parent_names FROM tbl_geographies t1
+			INNER JOIN tbl_geography_levels t2 on t1.level_id = t2.level_id
+			ORDER BY country_name, level_position, geography_name;
+	end if;
+END//
+DELIMITER;
