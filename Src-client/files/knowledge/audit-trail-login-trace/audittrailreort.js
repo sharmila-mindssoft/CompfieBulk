08391d29d2681ea7eb3c7eb431cdf1a27559aac4
usha <usha@mindssoft.com>
2016-11-30 10:17:43 +0530

    variables declared for the controls in audit trail forms

--- Src-client/files/knowledge/audit-trail-login-trace/audittrailreort.js
+++ Src-client/files/knowledge/audit-trail-login-trace/audittrailreort.js
@@ -18,8 +18,8 @@ Form_id = $('#formid');
 Form = $('#formname');
 CountryVal = $('#countryval');
 Country = $('#country')
-Category = $('#category');
-Show_btn = $(".btn-submit");
+Category = $('#categoryName');
+Show_btn = $("#show");
 
 //Autocomplete variable declaration
 var ACCountry = $('#ac-country');
@@ -31,9 +31,6 @@ var ItemsPerPage = $('#items_per_page');
 var PaginationView = $('.pagination-view');
 var Pagination = $('#pagination-rpt');
 var CompliacneCount = $('.compliance_count');
-var on_current_page = 1;
-var sno = 0;
-var totalRecord;
 
 a_page = null;
 function Auditpage() {
@@ -43,6 +40,9 @@ function Auditpage() {
     this._categoryList = {};
     this._countryList = {};
     this._auditData = {};
+    this._on_current_page = 1;
+    this._sno = 0;
+    this._total_record = 0;
 }
 
 Auditpage.prototype.displayMessage = function(message){
@@ -65,14 +65,14 @@ Auditpage.prototype.hideLoader = function() {
 
 Auditpage.prototype.resetFields = function(){
     $('.tbody-audittrail-list').find('tr').remove();
-    $('.grid-table').show();
+    $('.grid-table').hide();
     this._sno = 0;
     this.clearMessage();
 };
 
 Auditpage.prototype.getValue = function(field_name, f_id){
     if (field_name == "category") {
-        cg_id = Category_id.val().trim();
+        cg_id = Category.val();
         if (cg_id == '') {
             return null;
         }
@@ -148,6 +148,7 @@ Auditpage.prototype.validateMandatory = function(){
 };
 
 Auditpage.prototype.renderAuditData = function(a_page, audit_data){
+    $('.grid-table').show();
     $('.tbody-audittrail-list').find('tr').remove();
     showFrom = a_page._sno + 1;
     var is_null = true;
@@ -191,13 +192,14 @@ Auditpage.prototype.fetchData = function() {
     _form_id = this.getValue("form", null);
     _country_id = this.getValue("country", null);
     _category_id =this.getValue("category", null);
-    _page_limit = parseInt(Item_page_select.val());
+    _page_limit = parseInt(ItemsPerPage.val());
     if (this._on_current_page == 1) {
         _sno = 0
     }
     else {
         _sno = (this._on_current_page - 1) *  _page_limit;
     }
+    console.log(_from_date, _to_date, _user_id, _form_id, _country_id, _category_id, _sno, _page_limit)
     mirror.getAuditTrail(_from_date, _to_date, _user_id, _form_id, _country_id, _category_id, _sno, _page_limit,
         function(error, response) {
             if (error != null) {
@@ -206,10 +208,6 @@ Auditpage.prototype.fetchData = function() {
             else {
                 t_this._sno  = _sno;
                 t_this._auditData = response.audit_trail_details;
-                t_this._formList = response.forms;
-                t_this._userList = response.users;
-                t_this._categoryList = response.user_categories;
-                t_this._countryList = response.countries;
                 if (response.total_records == 0) {
                     t_this.hidePageView();
                     a_page.hidePagePan();
@@ -228,6 +226,144 @@ Auditpage.prototype.fetchData = function() {
     );
 };
 
+Auditpage.prototype.fetchFiltersData = function() {
+    console.log("fetchFiltersData")
+    var t_this = this;
+    mirror.getAuditTrailFilter(
+        function(error, response) {
+            if (error != null) {
+                this.displayMessage(error);
+            }
+            else {
+                console.log(response)
+                t_this._auditData = response.audit_trail_details;
+                t_this._formList = response.forms_list;
+                t_this._userList = response.users;
+                t_this._categoryList = response.user_categories;
+                t_this._countryList = response.audit_trail_countries;
+                t_this.setControlValues();
+            }
+        }
+    );
+};
+
+Auditpage.prototype.setControlValues = function(e) {
+    User.keyup(function(e) {
+        var newUserList = [];
+        if(Category.val() != '')
+        {
+            console.log(Category.val())
+            var textval = $(this).val();
+
+            for(var i=0;i<a_page._userList.length;i++)
+            {
+              if(a_page._userList[i].user_category_id == Category.val())
+              {
+                newUserList.push({
+                  "user_category_id": Category.val(),
+                  "user_id": a_page._userList[i].user_id,
+                  "employee_name": a_page._userList[i].employee_name
+                });
+              }
+            }
+            commonAutoComplete(
+                e, ACUser, User, textval,
+                newUserList, "employee_name", "user_id", function (val) {
+                onAutoCompleteSuccess(User, User_id, val);
+            });
+        }
+        else
+        {
+            displayMessage(message.user_required);
+        }
+    });
+
+    Form.keyup(function(e) {
+        var textval = $(this).val();
+        if(Category.val() != '')
+        {
+            var userId = User_id.val();
+            var form_list = [];
+            for(var i=0;i<a_page._userList.length;i++)
+            {
+                var user_check = userId>0?(userId == a_page._userList[i].user_id):false;
+
+                if((a_page._userList[i].user_category_id == Category.val()) &&
+                    (user_check == true || user_check == false))
+                {
+                    for(var j=0;j<a_page._auditData.length;j++)
+                    {
+                        if(a_page._auditData[j].user_id == a_page._userList[i].user_id)
+                        {
+                            console.log("inside audit_data")
+                            for(var k=0;k<a_page._formList.length;k++)
+                            {
+                                if(a_page._formList[k].form_id == a_page._auditData[j].form_id)
+                                {
+                                    console.log(a_page._formList[j].form_name)
+                                    form_list.push({
+                                        "form_id": a_page._formList[j].form_id,
+                                        "form_name": a_page._formList[j].form_name
+                                    });
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            commonAutoComplete(
+                e, ACForm, Form, textval,
+                form_list, "form_name", "form_id", function (val) {
+                onAutoCompleteSuccess(Form, Form_id, val);
+            });
+        }
+    });
+
+
+    Category.empty();
+    Category.append($('<option></option>').val('').html('Select'));
+    $.each(a_page._categoryList, function (key, value) {
+        Category.append($('<option></option>').val(a_page._categoryList[key].user_category_id).html(a_page._categoryList[key].user_category_name));
+    });
+
+    CountryVal.keyup(function (e) {
+        var textval = $(this).val();
+        if(Category.val() != '')
+        {
+            var userId = User_id.val();
+            var ctry_list = [];
+            for(var i=0;i<a_page._userList.length;i++)
+            {
+                var user_check = userId>0?(userId == a_page._userList[i].user_id):false;
+                console.log("user_check:"+user_check)
+                if((a_page._userList[i].user_category_id == Category.val()) &&
+                    (user_check == true || user_check == false))
+                {
+                    for(var j=0;j<a_page._countryList.length;j++)
+                    {
+                        console.log(a_page._countryList[j].user_id )
+                        if(a_page._countryList[j].user_id == a_page._userList[i].user_id)
+                        {
+                            ctry_list.push({
+                                "country_id": a_page._countryList[j].country_id,
+                                "country_name": a_page._countryList[j].country_name
+                            });
+                        }
+                    }
+                }
+            }
+            //var text_val = $(this).val();
+            commonAutoComplete(
+                e, ACCountry, Country, textval,
+                ctry_list, "country_name", "country_id", function (val) {
+                onAutoCompleteSuccess(CountryVal, Country, val);
+            });
+        }
+    });
+}
+
 Auditpage.prototype.renderControl = function(){
     To_date.val(current_date());
     From_date.val(past_days(7));  // 7 days bafore to_date
@@ -239,7 +375,7 @@ Auditpage.prototype.hidePageView = function() {
 };
 
 Auditpage.prototype.createPageView = function(a_obj, total_records) {
-    perPage = parseInt(Item_page_select.val());
+    perPage = parseInt(ItemsPerPage.val());
     a_obj.hidePageView();
     $('#pagination-rpt').twbsPagination({
         totalPages: Math.ceil(total_records/perPage),
@@ -265,119 +401,25 @@ Auditpage.prototype.hidePagePan = function() {
 
 Auditpage.prototype.renderPageControls = function(e) {
     var t_this = this;
-    Item_page_select.on('change', function(e) {
+    ItemsPerPage.on('change', function(e) {
         t_this.perPage = parseInt($(this).val());
         t_this._sno = 0;
         t_this._on_current_page = 1;
         t_this.createPageView(t_this, t_this._total_record);
         t_this.fetchData();
     });
-    t_this._perPage = parseInt(Item_page_select.val());
+    t_this._perPage = parseInt(ItemsPerPage.val());
 
 };
 
+//callback for autocomplete success
+function onAutoCompleteSuccess(value_element, id_element, val) {
+    value_element.val(val[1]);
+    id_element.val(val[0]);
+    value_element.focus();
+}
+
 initializeControlEvents = function(a_page){
-    User.keyup(function(e) {
-        var newUserList = [];
-        if(category.val() != '')
-        {
-            var textval = $(this).val();
-
-            for(var i=0;i<a_page._userList.length;i++)
-            {
-              if(a_page._userList[i].user_category_id == Category.val())
-              {
-                newUserList.push({
-                  "user_category_id": Category.val(),
-                  "user_id": a_page._userList[i].user_id,
-                  "employee_name": a_page._userList[i].employee_name
-                });
-              }
-            }
-            commonAutoComplete(
-                e, ACUser, User, textval,
-                a_page._userList, "employee_name", "user_id", function (val) {
-                onAutoCompleteSuccess(User, User_id, val);
-            });
-        }
-        else
-        {
-            displayMessage(message.user_required);
-        }
-    });
-
-    Form.keyup(function(e) {
-        var textval = $(this).val();
-        if(category.val() != '')
-        {
-            var userId = user_id.val();
-            var form_list = [];
-            for(var i=0;i<a_page._userList.length;i++)
-            {
-                var user_check = userId>0?(userId === a_page._userList[i].user_id):false;
-                if((a_page._userList[i].user_category_id == Category.val()) &&
-                    (user_check == true || user_check == false))
-                {
-                    for(var j=0;j<a_page._formList;j++)
-                    {
-                        if(a_page._formList[j].user_id == a_page._userList[i].user_id)
-                        {
-                            form_list.push({
-                                "form_id": a_page._formList[j].form_id,
-                                "form_name": a_page._formList[j].form_name
-                            });
-                        }
-                    }
-                }
-            }
-
-            commonAutoComplete(
-                e, ACForm, Form, textval,
-                form_list, "form_name", "form_id", function (val) {
-                onAutoCompleteSuccess(Form, Form_id, val);
-            });
-        }
-    });
-
-    Category.keyup(function(e) {
-        $.each(a_page._categoryList, function (key, value) {
-            Category.append($('<option></option>').val(a_page._categoryList[key].user_category_id).html(categoryList[key].user_category_name));
-        });
-    });
-
-    $('#countryval').keyup(function (e) {
-        var textval = $(this).val();
-        if(category.val() != '')
-        {
-            var userId = user_id.val();
-            var ctry_list = [];
-            for(var i=0;i<a_page._userList.length;i++)
-            {
-                var user_check = userId>0?(userId === a_page._userList[i].user_id):false;
-                if((a_page._userList[i].user_category_id == Category.val()) &&
-                    (user_check == true || user_check == false))
-                {
-                    for(var j=0;j<a_page._countryList;j++)
-                    {
-                        if(a_page._countryList[j].user_id == a_page._userList[i].user_id)
-                        {
-                            ctry_list.push({
-                                "country_id": a_page._countryList[j].country_id,
-                                "country_name": a_page._countryList[j].country_name
-                            });
-                        }
-                    }
-                }
-            }
-            var text_val = $(this).val();
-            commonAutoComplete(
-                e, ACCountry, Country, text_val,
-                countryList, "country_name", "country_id", function (val) {
-                onAutoCompleteSuccess(CountryVal, Country, val);
-            });
-        }
-    });
-
 
     Show_btn.click(function(e) {
         a_page.resetFields();
@@ -395,6 +437,7 @@ initializeControlEvents = function(a_page){
     on_page_load = function() {
         a_page.resetFields();
         is_valid = a_page.validateMandatory();
+        console.log("is_valid:"+is_valid)
         if (is_valid == true) {
             a_page.fetchData();
             a_page.renderPageControls();
@@ -408,6 +451,7 @@ a_page = new Auditpage();
 
 $(function () {
     loadItemsPerPage();
-    a_page.renderControl();
     initializeControlEvents(a_page);
+    a_page.fetchFiltersData();
+    a_page.renderControl();
 });
