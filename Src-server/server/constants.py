4bff238b32b50de2a4613b975e6305e03c914018
kodeeswaran-mindssoft <kodeeswaran@mindssoft.com>
2016-12-02 10:51:14 +0530

    update mysql pooling

--- Src-server/server/constants.py
+++ Src-server/server/constants.py
@@ -47,6 +47,6 @@ KNOWLEDGE_DB_USERNAME = "root"
 KNOWLEDGE_DB_PASSWORD = "123456"
 KNOWLEDGE_DATABASE_NAME = "compfie_knowledge_new"
 
-URL = "http://localhost:8082/"
+URL = "http://localhost:8081/"
 CLIENT_URL = URL
 KNOWLEDGE_URL = URL + "knowledge"
