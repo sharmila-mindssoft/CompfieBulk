682ea879955e84042e122d0efb70b8a1c0d2adfc
siva-mindssoft <siva@mindssoft.com>
2016-12-08 18:34:59 +0530

    update constant.py

--- Src-server/server/constants.py
+++ Src-server/server/constants.py
@@ -47,6 +47,6 @@ KNOWLEDGE_DB_USERNAME = "root"
 KNOWLEDGE_DB_PASSWORD = "123456"
 KNOWLEDGE_DATABASE_NAME = "compfie_knowledge_updated"
 
-URL = "http://localhost:8081/"
+URL = "http://localhost:8082/"
 CLIENT_URL = URL
 KNOWLEDGE_URL = URL + "knowledge"
