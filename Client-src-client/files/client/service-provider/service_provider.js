adba2a12923242daf0ece4dc4ac098f2be797978
jegan@mindssoft.com <jegan@mindssoft.com>
2017-01-10 11:36:05 +0530

    service provider master addition

---
+++ Client-src-client/files/client/service-provider/service_provider.js
@@ -0,0 +1,15 @@
+var AddScreen = $("#add-screen");
+var ViewScreen = $("#list-screen");
+var AddButton = $("#btn-add");
+var CancelScreen = $("#cancel-screen");
+var SubmitButton = $("#btn-submit");
+
+var ListContainer = $('#tbody-list');
+var FilterUserGroupName = $('#filter_service_provider');
+var FilterCategoryName = $('#filter-category-name');
+var FilterStatus = $('#filter-status');
+var CurrentPassword = $('#current-password');
+var PasswordSubmitButton = $('#password-submit');
+
+
+console.log("Hi") ;
