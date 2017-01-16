640001830156460ad795c496a9b1802c378918e4
siva-mindssoft <siva@mindssoft.com>
2017-01-16 16:39:33 +0530

    add filterable.js

--- Know-src-server/server/dbase.py
+++ Know-src-server/server/dbase.py
@@ -707,7 +707,7 @@ class Database(object):
 
     def save_activity(self, user_id, form_id, action):
         created_on = get_date_time()
-        q = "select user_category_id from tbl_users where user_id = %s"
+        q = "select user_category_id from tbl_user_login_details where user_id = %s"
         row = self.select_one(q, [user_id])
         user_cat_id = row.get("user_category_id")
 
