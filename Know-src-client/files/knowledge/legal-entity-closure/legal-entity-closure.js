de283af278b71f7c17163d2c5e943b0d217f416f
usha <usha@mindssoft.com>
2017-01-23 17:55:58 +0530

    3rd round bug rectification

--- Know-src-client/files/knowledge/legal-entity-closure/legal-entity-closure.js
+++ Know-src-client/files/knowledge/legal-entity-closure/legal-entity-closure.js
@@ -357,11 +357,9 @@ function renderSearch() {
     Search_status_li.each(function (index, el) {
       $(el).removeClass('active');
     });
-    console.log($(event.target).parent())
     $(event.target).parent().addClass('active');
 
-    var currentClass = $(event.target).find('i').attr('class');
-    alert(currentClass);
+    /*var currentClass = $(event.target).find('i').attr('class');
     Search_status.removeClass();
     if(currentClass != undefined){
       Search_status.addClass(currentClass);
@@ -369,7 +367,10 @@ function renderSearch() {
     }else{
       Search_status.addClass('fa');
       Search_status.text('All');
-    }
+    }*/
+    var currentClass = $(event.target).html();
+    Search_status.html(currentClass);
+
     processFilterSearch();
   });
 
@@ -379,15 +380,16 @@ function renderSearch() {
     });
     $(event.target).parent().addClass('active');
 
-    var currentClass = $(event.target).find('i').attr('class');
-    Search_status_1.removeClass();
+    var currentClass = $(event.target).html();
+    Search_status_1.html(currentClass);
+    /*Search_status_1.removeClass();
     if(currentClass != undefined){
       Search_status_1.addClass(currentClass);
       Search_status_1.text('');
     }else{
       Search_status_1.addClass('fa');
       Search_status_1.text('All');
-    }
+    }*/
     processFilterSearch();
   });
 }
