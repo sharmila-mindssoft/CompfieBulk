baae76d67bf72219355446685cec016b63a4519a
usha <usha@mindssoft.com>
2017-02-06 20:16:30 +0530

    api call for service provider wise report implemented

--- Client-src-client/files/client/js/client-mirror.js
+++ Client-src-client/files/client/js/client-mirror.js
@@ -2048,6 +2048,47 @@ function initClientMirror() {
         clientApiRequest(callerName, request, callback);
     }
 
+    /* Service Provider Wise report - updated*/
+    function getServiceProviderWiseReportFilters(country_id, le_id, callback) {
+        var request = [
+            'GetServiceProviderWiseReportFilters',
+            {
+                'country_id': country_id,
+                'legal_entity_id' : le_id
+            }
+        ];
+        callerName = 'client_reports';
+        clientApiRequest(callerName, request, callback);
+    }
+
+    function getServiceProviderWiseReport(
+        country_id, legal_entity_id, sp_id, domain_id, unit_id, statutory_mapping,
+        compliance_id, user_id, from_date, to_date,
+        task_status, csv, from_count, page_count, callback
+    ){
+        var request = [
+            'GetServiceProviderWiseReport',
+            {
+                'country_id': country_id,
+                'legal_entity_id' : legal_entity_id,
+                'sp_id': sp_id,
+                'domain_id': domain_id,
+                'unit_id': unit_id,
+                'statutory_mapping': statutory_mapping,
+                'compliance_id': compliance_id,
+                'user_id': user_id,
+                'due_from_date': from_date,
+                'due_to_date': to_date,
+                'task_status': task_status,
+                'csv': csv,
+                'from_count': from_count,
+                'page_count': page_count
+            }
+        ];
+        callerName = 'client_reports';
+        clientApiRequest(callerName, request, callback);
+    }
+
     return {
         log: log,
         toJSON: toJSON,
