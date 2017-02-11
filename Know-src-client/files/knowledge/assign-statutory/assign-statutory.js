e9b6953b8549042fd75cd3eaf08f7301db33b721
siva-mindssoft <siva@mindssoft.com>
2017-02-11 12:54:14 +0530

    assign statutory api response key change

--- Know-src-client/files/knowledge/assign-statutory/assign-statutory.js
+++ Know-src-client/files/knowledge/assign-statutory/assign-statutory.js
@@ -118,7 +118,7 @@ function callAPI(api_type) {
         displayLoader();
         mirror.getAssignedStatutories(function(error, data) {
             if (error == null) {
-                ASSIGNED_STATUTORIES = data.assigned_statutories;
+                ASSIGNED_STATUTORIES = data.assigned_statutories_approve;
                 loadAssignedStatutories();
 
             } else {
