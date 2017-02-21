b982bb8fb644570ecda686c6701f483704d86e49
Usha <usha@mindssoft.com>
2017-02-21 19:54:10 +0530

    Not complied and trend chart api added for widgets

--- Client-src-server/server/clientcontroller/widgetcontroller.py
+++ Client-src-server/server/clientcontroller/widgetcontroller.py
@@ -15,8 +15,10 @@ def process_client_widget_requests(request, db, session_user, session_category):
         result = get_escalation_count(db, le_ids, session_user, session_category)
 
     elif type(request) is widgetprotocol.GetNotCompliedChart :
-        pass
+        result = get_not_complied_count(db, session_user, session_category)
 
     elif type(request) is widgetprotocol.GetRiskChart :
-        result = get_risk_chart_count(db, le_ids, session_user, session_category)
+        result = get_risk_chart_count(db, session_user, session_category)
+    elif type(request) is widgetprotocol.GetTrendChart :
+        result = get_trend_chart(db, session_user, session_category)
     return result
