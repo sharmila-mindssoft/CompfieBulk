1891bfd5e9b63a51a62de97b312830c514f4fefa
Usha <usha@mindssoft.com>
2017-02-25 17:33:55 +0530

    group replication added.

--- Client-src-server/server/clientdatabase/savelegalentitydata.py
+++ Client-src-server/server/clientdatabase/savelegalentitydata.py
@@ -1,82 +1,47 @@
-import MySQLdb as mysql
 from server.dbase import Database
-from server.exceptionmessage import client_process_error
-from server.database.general import get_group_server_info
+
 __all__ = [
-    "LEntitydbConect",
-    "SaveUserData"
+    "LegalEntityReplicationManager",
 ]
 
-class LEntitydbConect(object):
-    def __init__(self):
-        self._k_db = None
+class LegalEntityReplicationManager(object):
+    def __init__(
+        self, group_db_info, time_out_seconds, callback
+    ):
+        self._group_db_info = group_db_info
+        self._time_out_seconds = time_out_seconds
+        self._callback = callback
+        self._first_time = True
 
-    def get_client_connect(self, db_host, db_port, db_username, db_password, db_name):
-        conn = mysql.connect(
-            host=db_host, port=db_port,
-            user=db_username, passwd=db_password,
-            db=db_name
-        )
-        print conn
-        self._k_db = Database(conn)
+    def _start(self):
+        self._poll()
 
-class SaveUserData(LEntitydbConect):
-    def __init__(self, request, le_ids):
-        super(SaveUserData, self).__init__()
-        self._request = request
-        self._le_id = le_ids
-        self.is_group = True
+    def _initiate_connction(self):
+        con = Database.make_connection(self._group_db_info)
+        _db = Database(con)
+        return _db
 
-        self._host = None
-        self._port = None
-        self._db_name = None
-        self._db_username = None
-        self._db_password = None
-        self.get_client_info()
-        self.process_save_token()
+    def _poll(self):
+        def on_timeout():
+            rows = []
+            _db = self._initiate_connction()
+            q = "select legal_entity_id, user_data, settings_data, provider_data " + \
+                " from tbl_le_replication_status where user_data = 1 or settings_data = 1 " + \
+                " provider_data = 1 "
+            try :
+                _db.begin()
+                rows = _db.select_all(q)
+            except Exception, e :
+                print e
+                _db.rollback()
 
-    def get_client_info(self):
-        rows = get_group_server_info(self.know_db, self.client_id)
-        if rows :
-            r = rows[0]
-            print r
-            self._host = r["database_ip"]
-            self._port = r["database_port"]
-            self._db_name = r["database_name"]
-            self._db_username = r["database_username"]
-            self._db_password = r["database_password"]
+            finally :
+                _db.close()
+                self._poll_response(rows)
 
-    def _save_token(self):
-        try :
-            qq = "select user_id from tbl_users where user_category_id = 1 and email_id = %s"
-            user_id_rows = self._k_db.select_all(qq, [self.email_id])
-            print user_id_rows
-            # print user_id_rows["user_id"]
-            if user_id_rows :
-                user_id = user_id_rows["user_id"]
-            else :
-                user_id = 1
+        if self._first_time :
+            self._first_time = False
+            on_timeout()
 
-            if user_id :
-                self._k_db.execute("delete from tbl_email_verification where user_id = %s and verification_type_id = 1", [user_id])
-                q = "insert into tbl_email_verification(user_id, verification_code, " + \
-                    " verification_type_id, expiry_date ) values(%s, %s, %s, %s)"
-                self._k_db.execute(q, [user_id, self.token, 1, self.expiry])
-
-        except Exception, e :
-            print e
-
-    def process_save_token(self):
-        try:
-            print self._host, self._port, self._db_username, self._db_password, self._db_name
-            self.get_client_connect(self._host, self._port, self._db_username, self._db_password, self._db_name)
-            # self._k_db.begin()
-            self._k_db._cursor = self._k_db._connection.cursor()
-            self._save_token()
-            self._k_db._cursor.close()
-            self._k_db._connection.commit()
-        except Exception, e:
-            print e
-            self._k_db._cursor.close()
-            self._k_db._connection.rollback()
-            raise client_process_error("E026")
+    def _poll_response(self, response):
+        self._callback(response)
--- Client-src-server/server/constants.py
+++ Client-src-server/server/constants.py
@@ -46,8 +46,8 @@ KNOWLEDGE_DB_HOST = "localhost"
 KNOWLEDGE_DB_PORT = 3306
 KNOWLEDGE_DB_USERNAME = "root"
 KNOWLEDGE_DB_PASSWORD = "123456"
-KNOWLEDGE_DATABASE_NAME = "compfie_knowledge_updated5"
+KNOWLEDGE_DATABASE_NAME = "compfie_knowledge_new"
 
-URL = "http://localhost:8082/"
+URL = "http://45.118.182.49:8080/"
 CLIENT_URL = URL
 KNOWLEDGE_URL = URL + "knowledge"
