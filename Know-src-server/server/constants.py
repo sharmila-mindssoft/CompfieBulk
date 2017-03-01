9f68822cf12cebdf05e8f5981ca4fe0f16dd176a
usha <usha@mindssoft.com>
2017-03-01 16:56:58 +0530

    database changed

--- Know-src-server/server/constants.py
+++ Know-src-server/server/constants.py
@@ -45,7 +45,7 @@ KNOWLEDGE_DB_PORT = 3306
 KNOWLEDGE_DB_USERNAME = "root"
 
 KNOWLEDGE_DB_PASSWORD = "123456"
-KNOWLEDGE_DATABASE_NAME = "compfie_knowledge_updated5"
+KNOWLEDGE_DATABASE_NAME = "compfie_knowledge_new"
 
 URL = "http://localhost:8082/"
 CLIENT_URL = URL
