50a263787fa865efe94800f109ed0e04cc36e54a
Usha <usha@mindssoft.com>
2017-03-24 15:55:36 +0530

    primary key validated.

--- Know-src-server/protocol/jsonvalidators.py
+++ Know-src-server/protocol/jsonvalidators.py
@@ -50,14 +50,24 @@ def parse_number(x, min_value, max_value):
         raise empty_error()
     if type(x) not in (int, long):
         raise expectation_error("a number", x)
-    if x >= min_value and x <= max_value:
-        return x
-    else:
-        msg = "a number between %s and %s" % (
-            min_value, max_value
-        )
-        # msg = "a number greater than 0"
-        raise expectation_error(msg, x)
+
+    if min_value is not None :
+        if x >= min_value :
+            return x
+
+    if max_value is not None :
+        if x <= max_value :
+            return x
+
+    if min_value is not None and max_value is not None :
+        if x >= min_value and x <= max_value:
+            return x
+        else:
+            msg = "a number between %s and %s" % (
+                min_value, max_value
+            )
+            # msg = "a number greater than 0"
+            raise expectation_error(msg, x)
 
 
 def parse_optional_number(x, min_value, max_value):
