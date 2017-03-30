bac23d22fb29a5ca31ba6cd75986ce4f309dcac9
siva-mindssoft <siva@mindssoft.com>
2017-03-30 13:19:13 +0530

    remove double comma in statutory setting list

--- Client-src-server/server/clientdatabase/clienttransaction.py
+++ Client-src-server/server/clientdatabase/clienttransaction.py
@@ -276,7 +276,7 @@ def get_statutory_settings(db, legal_entity_id, div_id, cat_id, session_user):
     user_cat_id = get_user_category(db, session_user)
 
     if user_cat_id <= 3 :
-        query = "select t1.unit_id, t1.unit_code, t1.unit_name, t1.postal_code,,  " + \
+        query = "select t1.unit_id, t1.unit_code, t1.unit_name, t1.postal_code,  " + \
             " t1.geography_name, t1.address , t2.domain_id, t3.domain_name, " + \
             " (select count(compliance_id) from tbl_client_compliances where " +\
             " unit_id = t1.unit_id and domain_id = t2.domain_id) as comp_count, " + \
@@ -535,7 +535,7 @@ def save_in_notification(
         db.insert("tbl_notifications_log", column, values)
         # for user_id in user_ids:
         save_notification_users(db, notification_id, user_ids)
-
+        
 def update_statutory_settings(db, data, session_user):
 
     domain_id = data.domain_id
@@ -584,7 +584,7 @@ def update_statutory_settings(db, data, session_user):
             usr_name = get_user_name_by_id(db, session_user)
             text = compliance_name + ' has been ' + opted_text + ' for ' + unit_name + ' by ' + usr_name
             save_in_notification(
-                db, domain_id, le_id, unit_id,
+                db, domain_id, le_id, unit_id, 
                 text, 4, user_ids
             )
 
@@ -1426,7 +1426,7 @@ def get_statutory_wise_compliances(
     # print "query>>>", query
     # print "para>>>", param
     rows = db.select_all(query, param)
-
+    
     level_1_statutory_wise_compliances = {}
     total_count = 0
     compliance_count = 0
@@ -1862,19 +1862,12 @@ def get_compliance_approval_list(
                     else:
                         file_name.append(file_name_part)
 
-        if row["concurred_by"] == None:
-            concurred_by_id = None
-        else:
-            concurred_by_id = None if(
-                int(row["concurred_by"]) == -1
-            ) else int(row["concurred_by"])
-
-        if row["approved_by"] == None:
-            approved_by_id = None
-        else:
-            approved_by_id = None if(
-                int(row["approved_by"]) == -1 or int(row["approved_by"]) == None
-            ) else int(row["approved_by"])
+        concurred_by_id = None if(
+            int(row["concurred_by"]) == -1
+        ) else int(row["concurred_by"])
+        approved_by_id = None if(
+            int(row["approved_by"]) == -1
+        ) else int(row["approved_by"])
 
         compliance_history_id = row["compliance_history_id"]
         start_date = datetime_to_string(row["start_date"])
