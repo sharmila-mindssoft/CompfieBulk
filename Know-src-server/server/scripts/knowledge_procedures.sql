c9ee2ab7a85e11cb1e8005053893ed480ee58255
Usha <usha@mindssoft.com>
2017-04-06 18:03:04 +0530

    legal entity country added in group db.

--- Know-src-server/server/scripts/knowledge_procedures.sql
+++ Know-src-server/server/scripts/knowledge_procedures.sql
@@ -9344,3 +9344,30 @@ END //
 
 DELIMITER ;
 
+
+DROP PROCEDURE IF EXISTS `sp_audit_trail_country_for_group`;
+
+DELIMITER //
+
+CREATE PROCEDURE `sp_audit_trail_country_for_group`(
+    IN le_id int(11), IN ct_id int(11)
+)
+BEGIN
+    INSERT INTO tbl_audit_log(action,
+                            client_id,
+                            legal_entity_id,
+                            tbl_auto_id,
+                            column_name,
+                            value,
+                            tbl_name)
+    select 0, 0, 0, t1.country_id, 'country_name', t1.country_name, 'tbl_countries'
+    from tbl_countries as t1
+    inner join tbl_legal_entities as t2 on t1.country_id = t2.country_id
+    where t2.legal_entity_id = le_id;
+
+    UPDATE tbl_client_replication_status set is_new_data = 1 where
+    client_id = ct_id and is_group = 1;
+
+END //
+
+DELIMITER ;
