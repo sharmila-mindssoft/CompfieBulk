6c8e09813a61a5490dd4f8fb382b18c70cb790ca
Usha <usha@mindssoft.com>
2017-04-06 16:49:03 +0530

    Review task start issue has been updated.

--- Know-src-server/processes/auto_start_task.py
+++ Know-src-server/processes/auto_start_task.py
@@ -144,6 +144,7 @@ class AutoStart(Database):
 
         logProcessInfo("compliance_to_start %s" % self.client_id, query % (self.current_date, self.current_date))
         rows = self.select_all(query, [self.current_date, self.current_date])
+        print query % (self.current_date, self.current_date)
         return rows
 
     def calculate_next_due_date(
@@ -157,6 +158,7 @@ class AutoStart(Database):
         trigger_before_days = None
         if statutory_dates == []:
             statutory_dates = None
+        print "FREQUENCY----------", frequency
         if frequency == 2 or frequency == 3 or frequency == 4:
             repeat_every = int(repeat_every)
             repeat_type = int(repeat_type)
@@ -306,6 +308,11 @@ class AutoStart(Database):
     def is_this_first_task_of_year(self, unit_id, country_id, domain_id, compliance_id):
         q = "select month_from, month_to from tbl_client_configuration where country_id = %s and domain_id = %s"
         c_row = self.select_one(q, [country_id, domain_id])
+
+        print "\n"
+        print q % (country_id, domain_id)
+        print c_row
+
         years = []
         if c_row["month_from"] == 1 and c_row["month_to"] == 12 :
             years = [getCurrentYear(), 1, getCurrentYear(), 2]
@@ -318,6 +325,8 @@ class AutoStart(Database):
         param = [unit_id, compliance_id]
         param.extend(years)
         rows = self.select_one(q1, param)
+        print q1 % tuple(param)
+        print rows
         if rows :
             cnt = rows["comp_count"]
         else :
@@ -326,10 +335,13 @@ class AutoStart(Database):
         statutory_date = None
         repeats_every = None
         repeats_type_id = None
-        if cnt > 0 :
+        if cnt == 0 :
+            print "count == 0"
             q1 = "select repeats_type_id, repeats_every, statutory_date, trigger_before_days, due_date from tbl_compliance_dates where " + \
                 "compliance_id = %s and unit_id = %s "
             d_rows = self.select_one(q1, [compliance_id, unit_id])
+            print q1 % (compliance_id, unit_id)
+            print d_rows
             if d_rows :
                 statutory_date = d_rows["repeats_type_id"]
                 repeats_every = d_rows["repeats_every"]
@@ -340,6 +352,17 @@ class AutoStart(Database):
                     qq = "update tbl_assign_compliances set statutory_dates = %s, trigger_before_days = %s, due_date = %s, " + \
                         " where compliance_id = %s and unit_id = %s"
                     self.execute(qq, [statutory_date, trigger_days, due_date, compliance_id, unit_id])
+        else :
+            q1 = "select repeats_type_id, repeats_every, statutory_dates, trigger_before_days, due_date from tbl_compliance_dates where " + \
+                "compliance_id = %s and unit_id = %s "
+            d_rows = self.select_one(q1, [compliance_id, unit_id])
+
+            if d_rows :
+                statutory_date = d_rows["repeats_type_id"]
+                repeats_every = d_rows["repeats_every"]
+                repeats_type_id = d_rows["statutory_date"]
+                due_date = d_rows["due_date"]
+                trigger_days = d_rows["trigger_before_days"]
 
         return cnt, statutory_date, repeats_every, repeats_type_id
 
@@ -404,6 +427,7 @@ class AutoStart(Database):
         data = self.get_compliance_to_start()
         count = 0
         for d in data :
+            print d["unit_id"]
             try :
                 approval_person = int(d["approval_person"])
                 if d["frequency_id"] == 1 :
@@ -418,7 +442,7 @@ class AutoStart(Database):
                 else:
                     if d["frequency_id"] in [3, 4] :
                         cnt, statutory_date, repeats_every, repeats_type_id = self.is_this_first_task_of_year(d["unit_id"], d["country_id"], d["domain_id"], d["compliance_id"])
-                        if cnt == 0 :
+                        if statutory_date is None and repeats_every is None and repeats_type_id is None :
                             continue
                     next_due_date = trigger_before = None
                     due_date = d["due_date"]
@@ -621,12 +645,11 @@ class AutoStart(Database):
         try :
             self.begin()
             self.start_new_task()
-            self.start_new_task()
             # self.check_service_provider_contract_period()
-            self.update_unit_wise_task_status()
-            self.update_user_wise_task_status()
-            self.update_duedate_in_calendar_view()
-            self.update_upcoming_in_calendar_view()
+            # self.update_unit_wise_task_status()
+            # self.update_user_wise_task_status()
+            # self.update_duedate_in_calendar_view()
+            # self.update_upcoming_in_calendar_view()
             self.commit()
             self.close()
         except Exception, e :
@@ -656,6 +679,7 @@ class DailyProcess(KnowledgeConnect):
                         c["database_port"], c["client_id"], c["legal_entity_id"], current_date
                     )
                     task.start_process()
+
                 except Exception, e :
                     logProcessError("DailyProcess", e)
                     logProcessError("DailyProcess", (traceback.format_exc()))
