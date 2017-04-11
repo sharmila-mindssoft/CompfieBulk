6ba6e17dd78be3eeb04bdeb5c73807ddae4d7d88
jegan@mindssoft.com <jegan@mindssoft.com>
2017-04-11 09:50:19 +0530

    message update for validity day setting checking

--- Client-src-client/files/client/js/message-properties.js
+++ Client-src-client/files/client/js/message-properties.js
@@ -429,7 +429,7 @@ var message = {
     'reassign_compliance_before_user_disable': 'User have compliances. Reassign compliances',
     "validity_settings_beyond": 'Validity Date beyond range',
     'new_contract_from_max_of_old_contract_to': 'New Contract From Date should be maximum of Old Contract To Date',
-    'validity_date_before_after': 'Validity Date must be Before/ After V_DAYS Days of Due Date',
+    'validity_date_before_after': 'Validity Date must be Before / After V_DAYS Days of Next Due Date',
     'repeats_every_less_equal_old_repeats_every': 'Repeats Every value is less than or equal to old Repeats Every value',
     'repeatevery_iszero': 'Repeats Every Value is not equal to 0',
 };
