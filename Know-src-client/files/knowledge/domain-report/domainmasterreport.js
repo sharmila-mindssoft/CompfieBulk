6d319c934cf15829b87c9790021c23dbbc6fd483
usha <usha@mindssoft.com>
2017-04-18 16:42:10 +0530

    QC, UAT Bugs rectified

--- Know-src-client/files/knowledge/domain-report/domainmasterreport.js
+++ Know-src-client/files/knowledge/domain-report/domainmasterreport.js
@@ -21,6 +21,13 @@ var sno = 0;
 var totalRecord;
 var ReportData;
 
+var Key = {
+  LEFT:   37,
+  UP:     38,
+  RIGHT:  39,
+  DOWN:   40
+};
+
 function displayLoader() {
   $('.loading-indicator-spin').show();
 }
@@ -44,6 +51,7 @@ function initialize() {
 
 function processSearch()
 {
+  searchList = [];
   usr_status = $('.search-status-li.active').attr('value');
 
     for (var entity in domainList) {
@@ -119,7 +127,7 @@ function renderControls(){
     $(event.target).parent().addClass('active');
 
     var currentClass = $(event.target).html();
-    Search_status_1.html(currentClass);
+    Search_status.html(currentClass);
 
     /*Search_status.removeClass();
     if(currentClass != undefined){
@@ -148,9 +156,10 @@ function renderControls(){
        event.preventDefault();
        return false;
     }*/
-    var k = e.which;
+    var k = e.which || e.keyCode;
       var ok = k >= 65 && k <= 90 || // A-Z
-          k >= 97 && k <= 122; // a-z
+          k >= 97 && k <= 122 || k == 46 || k ==8 || k == 9 || k == Key.LEFT ||
+                k == Key.RIGHT;  // a-z
           //k >= 48 && k <= 57; // 0-9
 
       if (!ok){
