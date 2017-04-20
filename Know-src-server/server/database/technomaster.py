8685977a533bed62d4f2f72d13c661b37762dfcd
kodeeswaran-mindssoft <kodeeswaran@mindssoft.com>
2017-04-20 11:19:47 +0530

    add comment line on technomaster

--- Know-src-server/server/database/technomaster.py
+++ Know-src-server/server/database/technomaster.py
@@ -1112,6 +1112,11 @@ def is_unit_exists_under_country(db, country, client_id):
         return False
 
 
+##########################################################################
+#  To Update client logo
+#  Parameters : Object of database, logo, client id
+#  Return Type : to save client logo
+##########################################################################
 def update_client_logo(db, logo, client_id):
     column = "logo_url"
     condition = "client_id = %s "
@@ -1125,6 +1130,12 @@ def update_client_logo(db, logo, client_id):
     return save_client_logo(logo)
 
 
+##########################################################################
+#  To Update client group
+#  Parameters : Object of database, client group details, 
+#  session user id
+#  Return Type : return true
+##########################################################################
 def update_client_group_record(db, client_group, session_user):
     current_time_stamp = get_date_time()
     contract_from = string_to_datetime(client_group.contract_from)
