61eafd590d5371278e3abb720cfdbf45ec5e624f
usha <usha@mindssoft.com>
2017-04-28 20:51:10 +0530

    bug rectification

--- Know-src-client/files/knowledge/geography-master/geographymaster.js
+++ Know-src-client/files/knowledge/geography-master/geographymaster.js
@@ -275,7 +275,7 @@ function loadGeographyFirstLevels(saverecord) {
   }
   $('#ulist' + setlevelstage).append(str);
   $('.addleft').on('input', function (e) {
-    this.value = isCommon($(this));
+    this.value = isCommon_Name($(this));
   });
 }
 //check & uncheck list data
