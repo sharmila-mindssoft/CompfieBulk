0b089c1237580607c1e172d1f96b0ec4da6eef83
siva-mindssoft <siva@mindssoft.com>
2017-04-28 13:45:02 +0530

    update sp_users_technouser_list procedure

--- Know-src-server/server/scripts/knowledge_procedures.sql
+++ Know-src-server/server/scripts/knowledge_procedures.sql
@@ -3962,14 +3962,12 @@ DELIMITER //
 
 CREATE PROCEDURE `sp_users_technouser_list`(session_user INT(11))
 BEGIN
-    select t1.user_id, t1.user_category_id, t1.employee_code, t1.employee_name, t1.is_active
-        from tbl_users as t1
-        inner join tbl_user_login_details as t2 on t1.user_id = t2.user_id
-        inner join tbl_user_mapping as t3 on t1.user_id = t3.child_user_id and t3.parent_user_id = session_user
-        where t1.is_active = 1
-        and t1.is_disable = 0
-        and t1.user_category_id = 6
-        group by user_id;
+    SELECT distinct t1.child_user_id as user_id, t2.is_active,
+        concat(t2.employee_code," - ", t2.employee_name) as employee_name
+        from tbl_user_mapping t1
+        INNER JOIN tbl_users t2 ON t1.child_user_id = t2.user_id AND t2.user_category_id = 6
+        AND t2.is_active = 1 AND t2.is_disable = 0
+        WHERE t1.parent_user_id = session_user;
 
     SELECT user_id, country_id FROM tbl_user_countries;
 
