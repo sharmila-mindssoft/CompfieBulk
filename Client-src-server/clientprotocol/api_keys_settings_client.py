4044c45b6d462696a38c93b4f9446ba17bc8d75b
Boginaathan <Boginaathan>
2017-06-17 03:25:42 +0530

    contract expire notification updated

--- Client-src-server/clientprotocol/api_keys_settings_client.py
+++ Client-src-server/clientprotocol/api_keys_settings_client.py
@@ -676,6 +676,7 @@ api_params = {
     "reminder_count": make_int_field(is_optional=True),
     "escalation_count": make_int_field(is_optional=True),
     "messages_count": make_int_field(is_optional=True),
+    "reminder_expire_count": make_int_field(is_optional=True),
 
     "pr_statutory_date": make_text_field(length=500, is_optional=True),
     "pr_compliances_1": make_vector_type_field(module="clienttransactions", klass_name="PAST_RECORD_COMPLIANCE"),
