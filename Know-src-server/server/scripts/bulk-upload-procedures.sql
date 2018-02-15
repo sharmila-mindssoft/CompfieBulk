9c37e7d6d73f5bdc399838049c33724edc807ce5
karthikeyan-mindssoft <kkarthik@mindssoft.com>
2018-02-15 16:54:28 +0530

    StatutoryMappingBulkReport - StoredProcedure

--- Know-src-server/server/scripts/bulk-upload-procedures.sql
+++ Know-src-server/server/scripts/bulk-upload-procedures.sql
@@ -104,3 +104,47 @@ BEGIN
 END //
 
 DELIMITER ;
+
+
+----------------------------------
+-- Statutory Mapping - Bulk Report
+----------------------------------
+DROP PROCEDURE IF EXISTS `sp_tbl_statutory_mappings_bulk_reportdata`;;
+CREATE PROCEDURE `sp_tbl_statutory_mappings_bulk_reportdata`(IN `user_id` varchar(100), IN `country_ids` varchar(100), IN `domain_ids` varchar(100), IN `from_date` date, IN `to_date` date, IN `from_limit` int(11), IN `to_limit` int(11))
+BEGIN
+ SELECT 
+t_sm_csv.country_name, 
+t_sm_csv.domain_name, 
+t_sm_csv.uploaded_by, 
+t_sm_csv.uploaded_on,
+t_sm_csv.csv_name, 
+t_sm_csv.total_records, 
+t_sm_csv.total_rejected_records, 
+t_sm_csv.approved_by, 
+t_sm_csv.rejected_by, 
+t_sm_csv.approved_on, 
+t_sm_csv.rejected_on, 
+t_sm_csv.is_fully_rejected,
+t_sm_csv.approve_status
+
+ FROM tbl_bulk_statutory_mapping AS t_sm
+ INNER JOIN tbl_bulk_statutory_mapping_csv AS t_sm_csv ON t_sm_csv.csv_id=t_sm.csv_id
+ WHERE 
+  t_sm_csv.uploaded_by=user_id
+  AND (DATE_FORMAT(date(t_sm_csv.uploaded_on),"%Y-%m-%d") BETWEEN date(from_date) and date(to_date))
+  AND FIND_IN_SET(t_sm_csv.domain_id, domain_ids)
+  AND FIND_IN_SET(t_sm_csv.country_id, country_ids)
+  ORDER BY t_sm_csv.uploaded_on DESC
+  LIMIT from_limit, to_limit;
+ 
+ SELECT count(0) as total
+ FROM tbl_bulk_statutory_mapping AS t_sm
+ INNER JOIN tbl_bulk_statutory_mapping_csv AS t_sm_csv ON t_sm_csv.csv_id=t_sm.csv_id
+ WHERE 
+  t_sm_csv.uploaded_by=user_id
+  AND (DATE_FORMAT(date(t_sm_csv.uploaded_on),"%Y-%m-%d") BETWEEN date(from_date) and date(to_date))
+  AND FIND_IN_SET(t_sm_csv.domain_id, domain_ids)
+  AND FIND_IN_SET(t_sm_csv.country_id, country_ids)
+  ORDER BY t_sm_csv.uploaded_on DESC;
+
+END;;
