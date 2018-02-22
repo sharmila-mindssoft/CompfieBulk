6693aa657968179dd9bd9a173c53e251667d3742
siva-mindssoft <siva@mindssoft.com>
2018-02-23 00:32:37 +0530

    assign statutory bulk upload front end file changes

--- Know-src-client/files/knowledge/bulk-upload/assign-statutory-bulk-upload/assign-statutory-bulk-upload.js
+++ Know-src-client/files/knowledge/bulk-upload/assign-statutory-bulk-upload/assign-statutory-bulk-upload.js
@@ -2,11 +2,6 @@
 var DownloaFileButton = $(".btn-download-file");
 var UploadFileButton = $(".btn-upload-file");
 
-var GROUPS = null;
-var LEGAL_ENTITIES = null;
-var UNITS = null;
-var DOMAINS = null;
-
 var GroupName = $('#group_name');
 var GroupId = $("#group_id");
 var ACGroup = $("#ac-group");
@@ -15,11 +10,25 @@ var LegalEntityName = $("#legal_entity_name");
 var LegalEntityId = $("#legal_entity_id");
 var ACLegalEntity = $("#ac-entity");
 
-
 var MultiSelect_Domain = $('#domains');
 var MultiSelect_Unit = $('#units');
-
 var UploadFile = $("#upload_file");
+
+var TotalRecordsCount = $('.totalRecords');
+var ValidRecordsCount = $('.validRecords');
+var InvalidRecordsCount = $('.invalidRecords');
+var MandatoryErrorsCount = $('.mandatoryErrors');
+var DuplicateErrorsCount = $('.duplicateErrors');
+var StatusErrorsCount = $('.statusErrors');
+var LengthErrorsCount = $('.lengthErrors');
+var InvalidErrorsCount = $('.invalidErrors');
+var InvalidFileName = null;
+
+
+var GROUPS = null;
+var LEGAL_ENTITIES = null;
+var UNITS = null;
+var DOMAINS = null;
 
 var u_names = [];
 var u_ids = [];
@@ -214,9 +223,8 @@ function pageControls() {
   });
 
     UploadFileButton.click(function() {
-        /*cl_id = GroupId.val();
+        cl_id = GroupId.val();
         le_id = LegalEntityId.val();
-        cl_name = GroupName.val();
         le_name = LegalEntityName.val();
         
     
@@ -242,45 +250,79 @@ function pageControls() {
                }
             });
 
-            u_names = [];
-            u_ids = [];
-            if(MultiSelect_Unit.val() == null){
-                for (var i in UNITS) {
-                    u_names.push(UNITS[i].u_name.split('-').pop());
-                    u_ids.push(UNITS[i].u_id)
-                }
-            }else{
-                $("#units option:selected").each(function () {
-                   var $this = $(this);
-                   if ($this.length) {
-                    var selText = $this.text().split('-').pop();
-                    u_names.push(selText);
-                   }
-                });
-                u_ids = MultiSelect_Unit.val().map(Number);
-            }*/
-
-        if (UploadFile.val() == null) {
-            displayMessage("Upload file required");
-            return false;
-        } else {
             var args = {
                 "csv_name": csvInfo["file_name"],
                 "csv_data": csvInfo["file_content"],
-                "csv_size": csvInfo["file_size"]
+                "csv_size": csvInfo["file_size"],
+                "cl_id": parseInt(cl_id), 
+                "le_id": parseInt(le_id), 
+                "d_ids": d_ids,
+                "le_name": le_name, 
+                "d_names": d_names
             };
             
             displayLoader();
             bu.getUploadAssignStatutoryCSV(args, function(error, data) {
                 if (error == null) {
+                    TotalRecordsCount.text(data.total);
+                    ValidRecordsCount.text(data.valid);
+                    InvalidRecordsCount.text(data.invalid);
+                    InvalidFileName = null;
+                    MandatoryErrorsCount.text("0");
+                    DuplicateErrorsCount.text("0");
+                    StatusErrorsCount.text("0");
+                    LengthErrorsCount.text("0");
+                    InvalidErrorsCount.text("0");
+                    $('.view-summary').show();
+                    $('.invaliddata').hide();
+                    displayMessage("Records uploaded successfully for approval");
                     hideLoader();
                 } else {
-                    displayMessage(error);
+                    InvalidFileName = data.invalid_file;
+                    TotalRecordsCount.text(data.total);
+                    var getValidCount = parseInt(data.total) - parseInt(data.invalid);
+                    ValidRecordsCount.text(data.getValidCount);
+                    InvalidRecordsCount.text(data.invalid);
+                    MandatoryErrorsCount.text(data.mandatory_error);
+                    DuplicateErrorsCount.text(data.duplicate_error);
+                    StatusErrorsCount.text(data.inactive_error);
+                    LengthErrorsCount.text(data.max_length_error);
+                    getInvaliddataCount = parseInt(data.invalid_char_error) + parseInt(data.invalid_data_error);
+                    InvalidErrorsCount.text(getInvaliddataCount);
+                    $('.invaliddata').show();
+                    $('.view-summary').show();
+                    download_file();
                     hideLoader();
                 }
             });
         }
+
     });
+}
+
+
+// To download the invalid files returned from validation
+
+function download_file() {
+    if(InvalidFileName != null) {
+        var splitFileName = InvalidFileName.split(".")[0];
+        console.log(splitFileName+".csv")
+        var downloadTag = $('.dropdown-content').find("a")
+        for(var i=0;i<downloadTag.length;i++) {
+            if(downloadTag[i].innerText == "Download Excel") {
+                $("#excel").attr("href", "http://" + window.location.host + "/bulkuploadinvalid/xlsx/" + splitFileName+".xlsx");
+            }
+            else if(downloadTag[i].innerText == "Download CSV") {
+                $("#csv").attr("href", "http://" + window.location.host + "/bulkuploadinvalid/csv/" + splitFileName+".csv");
+            }
+            else if(downloadTag[i].innerText == "Download ODS") {
+                $("#ods").attr("href", "http://" + window.location.host + "/bulkuploadinvalid/ods/" + splitFileName+".ods");
+            }
+            else if(downloadTag[i].innerText == "Download Text") {
+                $("#text").attr("href", "http://" + window.location.host + "/bulkuploadinvalid/text/" + splitFileName+".txt");
+            }
+        }
+    }
 }
 
 function initialize() {
