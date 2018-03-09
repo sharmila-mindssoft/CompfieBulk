ee02ed44f77a2681b84d470665c0b2869f289ddd
usha <usha@mindssoft.com>
2018-03-09 12:52:28 +0530

    added password key

--- Know-src-server/bulkupload/buapiprotocol/buapikeyssettings.py
+++ Know-src-server/bulkupload/buapiprotocol/buapikeyssettings.py
@@ -175,5 +175,5 @@ bu_api_params = {
     'rejected_unit_data': {'type': 'VECTOR_TYPE', 'length': None, 'validation_method': None, 'is_optional': False, 'module_name': client_units, "class_name": "ClientUnitRejectData"},
     'updated_unit_count': {'type': 'VECTOR_TYPE', 'length': None, 'validation_method': None, 'is_optional': False, 'module_name': client_units, "class_name": "UpdateUnitDownloadCount"},
     'pending_csv_list_as': {'type': 'VECTOR_TYPE', 'length': None, 'validation_method': None, 'is_optional': False, 'module_name': assign_statutory, "class_name": "PendingCsvListAssignStatutory"},
-
+    'password': {'type': 'STRING', 'length': 100, 'validation_method': None, 'is_optional': False},
 }
