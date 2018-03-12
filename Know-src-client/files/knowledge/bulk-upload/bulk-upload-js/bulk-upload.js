8a3ce4f62a39e6ff883701104a07b062d9e11714
boginaathanmindssoft <boginaathan@mindssoft.com>
2018-03-12 12:29:57 +0530

    Approve / Reject Assigned Statutory - Bulk Upload reject status updated

--- Know-src-client/files/knowledge/bulk-upload/bulk-upload-js/bulk-upload.js
+++ Know-src-client/files/knowledge/bulk-upload/bulk-upload-js/bulk-upload.js
@@ -491,7 +491,7 @@ function confirmClientUnitDeclination(csv_id, client_id, callback) {
   apiRequest("bu/client_units", request, callback);
 }
 
-function updateClientUnitActionFromView(csvid, b_u_id, action, remarks, callback){
+function updateClientUnitActionFromView(csvid, b_u_id, action, remarks, callback) {
   var request = [
     'SaveBulkClientUnitListFromView',
     {
@@ -502,7 +502,7 @@ function updateClientUnitActionFromView(csvid, b_u_id, action, remarks, callback
     }
   ];
   apiRequest("bu/client_units", request, callback);
-
+}
 
 function getApproveMappingViewFilterView(args, callback){
   var request = [
