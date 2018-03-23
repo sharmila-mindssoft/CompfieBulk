e2c1f6f9741e082fcd7c47795b6dafef55a5d95a
jegan@mindssoft.com <jegan@mindssoft.com>
2018-03-23 14:27:38 +0800

    updated

---
+++ Client-src-client/files/client/bulkupload/bulk-upload-js/client-bulk-upload-init.js
@@ -0,0 +1,11 @@
+function initMethods() {
+    return {
+        // getStatutoryMappingCsvList: getStatutoryMappingCsvList
+        // getCompletedTaskCurrentYearCsvList: getCompletedTaskCurrentYearCsvList,
+        // getDomains: getDomains,
+        UploadCompletedTaskCurrentYearCSV: UploadCompletedTaskCurrentYearCSV,
+        uploadCSVFile: uploadCSVFile
+    };
+}
+
+var buClient = initMethods();
