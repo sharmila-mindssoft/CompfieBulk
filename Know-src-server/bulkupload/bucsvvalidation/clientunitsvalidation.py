02916fa47b87a0cba4cf2c3a9f03ba6c6dc90616
Srivaishnavi <vaishnavi@mindssoft.com>
2018-05-07 18:20:38 +0530

    ! For Coding Standard !

--- Know-src-server/bulkupload/bucsvvalidation/clientunitsvalidation.py
+++ Know-src-server/bulkupload/bucsvvalidation/clientunitsvalidation.py
@@ -2216,7 +2216,7 @@ class ValidateClientUnitsBulkDataForApprove(SourceDB):
                         str("|;|".join(unit_error)),
                         int(unit_id)
                         ]
-                    )
+                )
             if action_type == 1:
                 q = "delete from tbl_bulk_units where action = %s and "\
                     "csv_unit_id = %s"
