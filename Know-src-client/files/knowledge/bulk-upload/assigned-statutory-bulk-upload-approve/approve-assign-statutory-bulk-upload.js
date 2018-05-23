e2663f154641a483cc0485b689099f4f17ee79d4
siva-mindssoft <siva@mindssoft.com>
2018-05-23 08:29:05 +0530

    hide download options div click outside

--- Know-src-client/files/knowledge/bulk-upload/assigned-statutory-bulk-upload-approve/approve-assign-statutory-bulk-upload.js
+++ Know-src-client/files/knowledge/bulk-upload/assigned-statutory-bulk-upload-approve/approve-assign-statutory-bulk-upload.js
@@ -1419,3 +1419,12 @@ $(document).ready(function() {
     bulkLoadItemsPerPage();
     $(".nicescroll").niceScroll();
 });
+
+$(document).mouseup(function(e) 
+{
+    var container = $(".dropdown-content");
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
+    {
+        container.hide();
+    }
+});
