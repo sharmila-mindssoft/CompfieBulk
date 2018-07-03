1593c82f85a5e979c1faeee367454e15ba7a8c8a
Sharmila <sharmila@mindssoft.com>
2018-07-03 18:39:33 +0530

    Fixed the following bugs: 1. try exception handling while submitting and processing queued task - Finished 2. delete txt file permission (777) - Finished 3. catch db errors  - Finished 4. compliance description - not found - Finished 5. change the MiB to MB in dropzone.js - Finished 6. check duplicate due date with in csv using provision-Finished

--- Client-src-client/files/client/bulkupload/bulk-upload-js/dropzone.js
+++ Client-src-client/files/client/bulkupload/bulk-upload-js/dropzone.js
@@ -437,7 +437,7 @@ var Dropzone = function (_Emitter) {
          * If the filesize is too big.
          * `{{filesize}}` and `{{maxFilesize}}` will be replaced with the respective configuration values.
          */
-        dictFileTooBig: "File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.",
+        dictFileTooBig: "File is too big ({{filesize}}MB). Max filesize: {{maxFilesize}}MB.",
 
         /**
          * If the file doesn't match the file type.
--- Client-src-server/bulkupload/budatabase/bucompletedtaskcurrentyeardb.py
+++ Client-src-server/bulkupload/budatabase/bucompletedtaskcurrentyeardb.py
@@ -79,37 +79,37 @@ def save_completed_task_current_year_csv(completed_task):
 
 
 def save_completed_task_data(csv_id, csv_data):
-    try:
-        columns = [
-            "csv_past_id", "Legal_Entity", "Domain",
-            "Unit_Code", "Unit_Name", "perimary_legislation",
-            "Secondary_Legislation", "statutory_provision", "compliance_task_name",
-            "Compliance_Description", "Compliance_Frequency", "Statutory_Date",
-            "Due_Date", "Assignee", "Completion_Date", "document_name"
-        ]
+    columns = [
+        "csv_past_id", "Legal_Entity", "Domain",
+        "Unit_Code", "Unit_Name", "perimary_legislation",
+        "Secondary_Legislation", "statutory_provision", "compliance_task_name",
+        "Compliance_Description", "Compliance_Frequency", "Statutory_Date",
+        "Due_Date", "Assignee", "Completion_Date", "document_name"
+    ]
 
-        values = []
-        for idx, d in enumerate(csv_data):
-            values.append((
-                csv_id, d["Legal_Entity"],
-                d["Domain"], d["Unit_Code"], d["Unit_Name"],
-                d["Primary_Legislation"], d["Secondary_Legislation"],
-                d["Statutory_Provision"],
-                d["Compliance_Task"], d["Compliance_Description"],
-                d["Compliance_Frequency"], d["Statutory_Date"],
-                string_to_datetime(d["Due_Date"]), d["Assignee"],
-                string_to_datetime(d["Completion_Date"]), d["Document_Name"]
+    values = []
+    for idx, d in enumerate(csv_data):
+        values.append((
+            csv_id, d["Legal_Entity"],
+            d["Domain"], d["Unit_Code"], d["Unit_Name"],
+            d["Primary_Legislation"], d["Secondary_Legislation"],
+            d["Statutory_Provision"],
+            d["Compliance_Task"], d["Compliance_Description"],
+            d["Compliance_Frequency"], d["Statutory_Date"],
+            string_to_datetime(d["Due_Date"]), d["Assignee"],
+            string_to_datetime(d["Completion_Date"]), d["Document_Name"]
 
-            ))
-        db = connect_bulk_db()
-        if values:
-            db.bulk_insert("tbl_bulk_past_data", columns, values)
-            db.commit()
-            return True
-        else:
-            return False
-    except Exception:
-        raise ValueError("Transaction failed")
+        ))
+    if values:
+        try:
+            db = connect_bulk_db()
+            if db.bulk_insert("tbl_bulk_past_data", columns, values):
+                db.commit()
+                return True
+            else:
+                raise ValueError("Transaction Failed")
+        except Exception, e:
+            return e
 
 
 def get_past_record_data(db, csv_id):
